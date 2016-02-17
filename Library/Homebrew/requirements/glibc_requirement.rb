43fd4316be934e4880f35de254cc13b257aa2172
Shaun Jackman <sjackman@gmail.com>
2016-02-17 11:36:44 -0800

    Factor out GlibcRequirement.system_version

--- Library/Homebrew/requirements/glibc_requirement.rb
+++ Library/Homebrew/requirements/glibc_requirement.rb
@@ -3,11 +3,23 @@ require "requirement"
 class GlibcRequirement < Requirement
   fatal true
   default_formula "glibc"
+  @@system_version = nil
 
   def initialize
     # Bottles for Linuxbrew are built using glibc 2.19.
     @version = "2.19"
     super
+  end
+
+  def self.system_version
+    return @@system_version if @@system_version
+    libc = ["/lib/x86_64-linux-gnu/libc.so.6", "/lib64/libc.so.6"].find do |s|
+      Pathname.new(s).executable?
+    end
+    raise "Unable to locate the system's glibc" unless libc
+    version = Utils.popen_read(libc)[/version (\d\.\d+)/, 1]
+    raise "Unable to determine the system's glibc version" unless version
+    @@system_version = version
   end
 
   satisfy(:build_env => false) {
@@ -18,12 +30,6 @@ class GlibcRequirement < Requirement
       # Fix for brew tests, which uses NullLoader.
       true
     end
-    libc = ["/lib/x86_64-linux-gnu/libc.so.6", "/lib64/libc.so.6"].find do |s|
-      Pathname.new(s).executable?
-    end
-    next false unless libc
-    version = Utils.popen_read(libc)[/version (\d\.\d+)/, 1]
-    next false unless version
-    Version.new(version.to_s) >= Version.new(@version)
+    Version.new(system_version.to_s) >= Version.new(@version)
   }
 end
