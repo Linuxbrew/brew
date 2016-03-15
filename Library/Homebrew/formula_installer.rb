af50515e0f180e476b7db7022865c9d162b44c32
Shaun Jackman <sjackman@gmail.com>
2016-03-15 13:55:14 -0700

    bottle_dependencies: Install both glibc and gcc
    Installing bottles on Linux require a recent version of glibc and gcc.
    GCC is required for libgcc_s.so and libstdc++.so. It depends on glibc.
    
    Closes Linuxbrew/linuxbrew#941.

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -352,12 +352,11 @@ class FormulaInstaller
     # executables and shared libraries on Linux.
     deps << Dependency.new("patchelf")
 
-    # Installing bottles on Linux require a recent version of glibc.
-    glibc = GlibcRequirement.new
-    unless glibc.satisfied?
-      glibc_dep = glibc.to_dependency
-      glibc_f = glibc_dep.to_formula
-      deps += Dependency.expand(glibc_f) << glibc_dep
+    # Installing bottles on Linux require a recent version of glibc and gcc.
+    # GCC is required for libgcc_s.so and libstdc++.so. It depends on glibc.
+    unless GlibcRequirement.new.satisfied?
+      gcc_dep = Dependency.new("gcc")
+      deps += Dependency.expand(gcc_dep.to_formula) << gcc_dep
     end
 
     deps = deps.select do |dep|
