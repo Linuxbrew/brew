312fdaa892bce745f909215f81f3257d170c0f5a
Martin Afanasjew <martin@afanasjew.de>
2016-04-19 03:04:49 +0200

    --version: move help to documentation comment
    This implies that `--version` is treated in most places like a regular
    command, e.g. being suggested in shell completion. Also fix the help
    text that claimed output goes to standard error, while it actually goes
    to standard output.

---
+++ Library/Homebrew/cmd/--version.rb
@@ -0,0 +1,12 @@
+#:  * `--version`:
+#:    Print the version number of Homebrew to standard output and exit.
+
+module Homebrew
+  def __version
+    # As a special case, `--version` is implemented directly in `brew.rb`. This
+    # file merely serves as a container for the documentation. It also catches
+    # the case where running `brew --version` with additional arguments would
+    # produce a rather cryptic message about a non-existent `--version` command.
+    raise UsageError
+  end
+end
