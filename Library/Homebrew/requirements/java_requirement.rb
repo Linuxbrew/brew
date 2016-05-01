3972b900f34f578ed9bde9cd66ea79f28b26e2d2
Xu Cheng <xucheng@me.com>
2016-05-01 14:31:26 +0800

    JavaRequirement: use next instead of return in code block

--- Library/Homebrew/requirements/java_requirement.rb
+++ Library/Homebrew/requirements/java_requirement.rb
@@ -6,7 +6,7 @@ class JavaRequirement < Requirement
   download "http://www.oracle.com/technetwork/java/javase/downloads/index.html"
 
   satisfy :build_env => false do
-    return false unless File.executable? "/usr/libexec/java_home"
+    next false unless File.executable? "/usr/libexec/java_home"
 
     args = %w[--failfast]
     args << "--version" << "#{@version}" if @version
