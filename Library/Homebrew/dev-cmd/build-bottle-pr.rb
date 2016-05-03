538ef438533622e5e5919dbb2327dd7047d37953
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2016-05-03 17:36:15 +0200

    build-bottle-pr: Specify the head of the PR
    Get the GitHub username from GITHUB_USER.
    Fix a style issue.
    
    Closes Linuxbrew/brew#13.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Library/Homebrew/dev-cmd/build-bottle-pr.rb
+++ Library/Homebrew/dev-cmd/build-bottle-pr.rb
@@ -5,8 +5,8 @@ module Homebrew
   # Options:
   #    --remote=$USER      Specify the GitHub remote
   #    --tag=x86_64_linux  Specify the bottle tag
-  def build_bottle formula
-    remote = ARGV.value("remote") || ENV["USER"]
+  def build_bottle(formula)
+    remote = ARGV.value("remote") || ENV["GITHUB_USER"] || ENV["USER"]
     tag = (ARGV.value("tag") || "x86_64_linux").to_sym
     return ohai "#{formula}: Skipping because a bottle is not needed" if formula.bottle_unneeded?
     return ohai "#{formula}: Skipping because bottles are disabled" if formula.bottle_disabled?
@@ -25,7 +25,8 @@ module Homebrew
     safe_system "git", "checkout", "-b", branch, "master"
     safe_system "git", "commit", formula.path, "-m", message
     safe_system "git", "push", remote, branch
-    safe_system "hub", "pull-request", "--browse", "-m", message
+    safe_system "hub", "pull-request", "--browse",
+      "-h", "#{remote}:#{branch}", "-m", message
     safe_system "git", "checkout", "master"
     safe_system "git", "branch", "-D", branch
   end
