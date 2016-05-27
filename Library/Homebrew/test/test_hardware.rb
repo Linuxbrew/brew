68c10e47400571d20c1c6dc407a7d9b7c32ba79a
Shaun Jackman <sjackman@gmail.com>
2016-05-27 07:22:56 -0700

    LinuxCPUs: Always use -march=native for Linuxbrew
    Closes Linuxbrew/brew#54.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Library/Homebrew/test/test_hardware.rb
+++ Library/Homebrew/test/test_hardware.rb
@@ -7,7 +7,9 @@ class HardwareTests < Homebrew::TestCase
   end
 
   def test_hardware_intel_family
-    families = [:core, :core2, :penryn, :nehalem, :arrandale, :sandybridge, :ivybridge, :haswell, :broadwell, :skylake]
+    families = [
+      :core, :core2, :penryn, :nehalem, :arrandale, :sandybridge, :ivybridge, :haswell, :broadwell, :skylake,
+      :westmere, :merom, :dothan, :atom, :presler, :prescott, :arm]
     assert_includes families, Hardware::CPU.family
-  end if Hardware::CPU.intel?
+  end
 end
