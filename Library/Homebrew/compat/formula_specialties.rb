e107c86aecc8f53a254c0e8582a5db87c1bd7fdf
Mike McQuaid <mike@mikemcquaid.com>
2016-07-16 22:15:23 +0100

    compat/formula_specialities: cleanup style.

--- Library/Homebrew/compat/formula_specialties.rb
+++ Library/Homebrew/compat/formula_specialties.rb
@@ -1,15 +1,13 @@
-# Base classes for specialized types of formulae.
-
-# See chcase for an example
 class ScriptFileFormula < Formula
   def install
+    odeprecated "ScriptFileFormula#install", "Formula#install"
     bin.install Dir["*"]
   end
 end
 
-# See browser for an example
 class GithubGistFormula < ScriptFileFormula
   def self.url(val)
+    odeprecated "GithubGistFormula.url", "Formula.url"
     super
     version File.basename(File.dirname(val))[0, 6]
   end
@@ -21,6 +19,8 @@ class AmazonWebServicesFormula < Formula
   # Use this method to peform a standard install for Java-based tools,
   # keeping the .jars out of HOMEBREW_PREFIX/lib
   def install
+    odeprecated "AmazonWebServicesFormula#install", "Formula#install"
+
     rm Dir["bin/*.cmd"] # Remove Windows versions
     libexec.install Dir["*"]
     bin.install_symlink Dir["#{libexec}/bin/*"] - ["#{libexec}/bin/service"]
@@ -29,6 +29,8 @@ class AmazonWebServicesFormula < Formula
 
   # Use this method to generate standard caveats.
   def standard_instructions(home_name, home_value = libexec)
+    odeprecated "AmazonWebServicesFormula#standard_instructions", "Formula#caveats"
+
     <<-EOS.undent
       Before you can use these tools you must export some variables to your $SHELL.
 
