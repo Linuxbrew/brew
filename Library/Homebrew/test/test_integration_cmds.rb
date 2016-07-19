4f95f7ff84eb6a65b4b6d08af90d1e40e2ccb4fe
Andrea Kao <eirinikos@gmail.com>
2016-07-19 08:33:36 -0700

    tests: add cmd/irb integration test (#501)

--- Library/Homebrew/test/test_integration_cmds.rb
+++ Library/Homebrew/test/test_integration_cmds.rb
@@ -690,4 +690,20 @@ class IntegrationCommandTests < Homebrew::TestCase
     cmd("install", "testball")
     assert_match "Would remove", cmd("unlink", "--dry-run", "testball")
   end
+
+  def test_irb
+    assert_match "'v8'.f # => instance of the v8 formula",
+      cmd("irb", "--examples")
+
+    setup_test_formula "testball"
+
+    irb_test = HOMEBREW_TEMP/"irb-test.rb"
+    irb_test.write <<-EOS.undent
+      "testball".f
+      :testball.f
+      exit
+    EOS
+
+    assert_match "Interactive Homebrew Shell", cmd("irb", irb_test)
+  end
 end
