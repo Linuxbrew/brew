05daa0574732a7884bd158b2c3e14bd0709367da
AnastasiaSulyagina <anastasia.sulyagina@gmail.com>
2016-07-20 21:15:20 +0300

    tap: add cask methods.
    Closes #562.

--- Library/Homebrew/tap.rb
+++ Library/Homebrew/tap.rb
@@ -72,6 +72,7 @@ class Tap
   def clear_cache
     @remote = nil
     @formula_dir = nil
+    @cask_dir = nil
     @formula_files = nil
     @alias_dir = nil
     @alias_files = nil
@@ -304,6 +305,11 @@ class Tap
     @formula_dir ||= [path/"Formula", path/"HomebrewFormula", path].detect(&:directory?)
   end
 
+  # path to the directory of all casks for caskroom/cask {Tap}.
+  def cask_dir
+    @cask_dir ||= [path/"Casks", path].detect(&:directory?)
+  end
+
   # an array of all {Formula} files of this {Tap}.
   def formula_files
     @formula_files ||= if formula_dir
@@ -319,7 +325,7 @@ class Tap
   def formula_file?(file)
     file = Pathname.new(file) unless file.is_a? Pathname
     file = file.expand_path(path)
-    file.extname == ".rb" && file.parent == formula_dir
+    file.extname == ".rb" && (file.parent == formula_dir || file.parent == cask_dir)
   end
 
   # an array of all {Formula} names of this {Tap}.
@@ -557,6 +563,14 @@ class CoreTap < Tap
   # @private
   def formula_dir
     @formula_dir ||= begin
+      self.class.ensure_installed!
+      super
+    end
+  end
+
+  # @private
+  def cask_dir
+    @cask_dir ||= begin
       self.class.ensure_installed!
       super
     end
