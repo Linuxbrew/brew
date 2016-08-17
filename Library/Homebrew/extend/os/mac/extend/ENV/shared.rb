27fc2abacabd0990cce91eca42cc63ba05866e65
Mike McQuaid <mike@mikemcquaid.com>
2016-08-17 13:23:13 +0100

    shared: handle missing Xcode/CLT version.
    Fixes https://github.com/Homebrew/homebrew-core/issues/3973.

--- Library/Homebrew/extend/os/mac/extend/ENV/shared.rb
+++ Library/Homebrew/extend/os/mac/extend/ENV/shared.rb
@@ -1,6 +1,15 @@
 module SharedEnvExtension
   def no_weak_imports?
     return false unless compiler == :clang
-    MacOS::Xcode.version >= "8.0" || MacOS::CLT.version >= "8.0"
+
+    if MacOS::Xcode.version && MacOS::Xcode.version < "8.0"
+      return false
+    end
+
+    if MacOS::CLT.version && MacOS::CLT.version < "8.0"
+      return false
+    end
+
+    true
   end
 end
