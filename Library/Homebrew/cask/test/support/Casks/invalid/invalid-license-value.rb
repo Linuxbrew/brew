3d0e7a572fa283c051e09bee8a245e3089eadb58
Markus Reiter <me@reitermark.us>
2016-08-20 03:40:03 +0200

    Dummy Transmission app.

--- Library/Homebrew/cask/test/support/Casks/invalid/invalid-license-value.rb
+++ Library/Homebrew/cask/test/support/Casks/invalid/invalid-license-value.rb
@@ -1,6 +1,6 @@
 test_cask 'invalid-license-value' do
   version '2.61'
-  sha256 'd26d7481cf1229f879c05e11cbdf440d99db6d6342f26c73d8ba7861b975532f'
+  sha256 'e44ffa103fbf83f55c8d0b1bea309a43b2880798dae8620b1ee8da5e1095ec68'
 
   url TestHelper.local_binary_url('transmission-2.61.dmg')
   homepage 'http://example.com/invalid-license-value'
