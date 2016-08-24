03352805c66c8f545fad59f7007f615a55d3e37b
Mike McQuaid <mike@mikemcquaid.com>
2016-08-24 11:06:33 +0100

    tap: use utils link_path_manpages.

--- Library/Homebrew/tap.rb
+++ Library/Homebrew/tap.rb
@@ -248,26 +248,7 @@ class Tap
   end
 
   def link_manpages
-    return unless (path/"man").exist?
-    conflicts = []
-    (path/"man").find do |src|
-      next if src.directory?
-      dst = HOMEBREW_PREFIX/"share"/src.relative_path_from(path)
-      next if dst.symlink? && src == dst.resolved_path
-      if dst.exist?
-        conflicts << dst
-        next
-      end
-      dst.make_relative_symlink(src)
-    end
-    unless conflicts.empty?
-      onoe <<-EOS.undent
-        Could not link #{name} manpages to:
-          #{conflicts.join("\n")}
-
-        Please delete these files and run `brew tap --repair`.
-      EOS
-    end
+    link_path_manpages(path, "brew tap --repair")
   end
 
   # uninstall this {Tap}.
