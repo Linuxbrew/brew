37beacd37d76a069c3819d5c67e07d15a9934069
Alyssa Ross <hi@alyssa.is>
2016-09-07 20:24:02 +0100

    Revert "Implement opoo in bash"
    This reverts commit 2498d99a143d0e50e92a0aa60316d47c9ea83edc.

--- Library/Homebrew/brew.sh
+++ Library/Homebrew/brew.sh
@@ -1,33 +1,18 @@
 HOMEBREW_VERSION="0.9.9"
 
-o() {
+onoe() {
+  if [[ -t 2 ]] # check whether stderr is a tty.
+  then
+    echo -ne "\033[4;31mError\033[0m: " >&2 # highlight Error with underline and red color
+  else
+    echo -n "Error: " >&2
+  fi
   if [[ $# -eq 0 ]]
   then
-    /bin/cat
+    /bin/cat >&2
   else
-    echo "$*"
+    echo "$*" >&2
   fi
-}
-
-highlight() {
-  if [[ -t 1 ]] # check whether stdout is a tty.
-  then
-    echo -ne "\033[4;$1m$2\033[0m"
-  else
-    echo -n "$2"
-  fi
-}
-
-opoo() {
-  highlight 33 Warning >&2 # yellow
-  echo -n ": " >&2
-  o "$@" >&2
-}
-
-onoe() {
-  highlight 31 Error >&2 # red
-  echo -n ": " >&2
-  o "$@" >&2
 }
 
 odie() {
