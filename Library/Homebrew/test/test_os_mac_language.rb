0243e1f9a70c6bb1304dacc3abbb0d0d0ca383b2
Markus Reiter <me@reitermark.us>
2016-09-09 00:11:43 +0200

    Add language method to `OS::Mac`.

---
+++ Library/Homebrew/test/test_os_mac_language.rb
@@ -0,0 +1,8 @@
+require "testing_env"
+require "os/mac"
+
+class OSMacLanguageTests < Homebrew::TestCase
+  def test_language_format
+    assert_match %r{\A[a-z]{2}(-[A-Z]{2})?\Z}, OS::Mac.language
+  end
+end
