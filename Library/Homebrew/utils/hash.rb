1f2abbdd6e960399b50f35e3e34261d13b9cf893
Mike McQuaid <mike@mikemcquaid.com>
2016-09-11 17:47:04 +0100

    utils/hash: fix Rubocop warnings.

--- Library/Homebrew/utils/hash.rb
+++ Library/Homebrew/utils/hash.rb
@@ -1,6 +1,6 @@
 def deep_merge_hashes(hash1, hash2)
-  merger = proc do |key, v1, v2|
-    if Hash === v1 && Hash === v2
+  merger = proc do |_key, v1, v2|
+    if v1.is_a?(Hash) && v2.is_a?(Hash)
       v1.merge v2, &merger
     else
       v2
