3bf5e779ab04bf399963183eb59f815b5df77c5d
Mike McQuaid <mike@mikemcquaid.com>
2016-09-11 17:47:04 +0100

    utils/curl: fix Rubocop warnings.

--- Library/Homebrew/utils/curl.rb
+++ Library/Homebrew/utils/curl.rb
@@ -1,7 +1,7 @@
 require "pathname"
 require "open3"
 
-def curl_args(extra_args=[])
+def curl_args(extra_args = [])
   curl = Pathname.new ENV["HOMEBREW_CURL"]
   curl = Pathname.new "/usr/bin/curl" unless curl.exist?
   raise "#{curl} is not executable" unless curl.exist? && curl.executable?
@@ -9,7 +9,7 @@ def curl_args(extra_args=[])
   flags = HOMEBREW_CURL_ARGS
   flags -= ["--progress-bar"] if ARGV.verbose?
 
-  args = ["#{curl}"] + flags + extra_args
+  args = [curl.to_s] + flags + extra_args
   args << "--verbose" if ENV["HOMEBREW_CURL_VERBOSE"]
   args << "--silent" if !$stdout.tty? || ENV["TRAVIS"]
   args
