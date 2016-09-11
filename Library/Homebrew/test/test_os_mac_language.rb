501774e3bc73d5dd367054b954893d9561bf00d2
Mike McQuaid <mike@mikemcquaid.com>
2016-09-11 17:43:09 +0100

    test/test_os_mac_language: fix Rubocop warnings.

--- Library/Homebrew/test/test_os_mac_language.rb
+++ Library/Homebrew/test/test_os_mac_language.rb
@@ -3,6 +3,6 @@ require "os/mac"
 
 class OSMacLanguageTests < Homebrew::TestCase
   def test_language_format
-    assert_match %r{\A[a-z]{2}(-[A-Z]{2})?\Z}, OS::Mac.language
+    assert_match(/\A[a-z]{2}(-[A-Z]{2})?\Z/, OS::Mac.language)
   end
 end
