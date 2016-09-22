57c2c560a123157d4fd8e00b31a42ceff2f73eaf
Bob W. Hogg <rwhogg@linux.com>
2016-09-22 08:36:10 -0400

    one less block in mac dep

--- Library/Homebrew/extend/os/mac/dependency_collector.rb
+++ Library/Homebrew/extend/os/mac/dependency_collector.rb
@@ -1,7 +1,6 @@
 class DependencyCollector
   def ant_dep(spec, tags)
-    if MacOS.version >= :mavericks
-      Dependency.new(spec.to_s, tags)
-    end
+    return if MacOS.version < :mavericks
+    Dependency.new(spec.to_s, tags)
   end
 end
