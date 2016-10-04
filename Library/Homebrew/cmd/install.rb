e29700cbee33d47f3a158ee76debf8639b1ac447
Ryan Sharif <ryan@FriendlyARM.socal.rr.com>
2016-10-04 03:51:12 +0000

    fixed ld.so missing on armhf

--- Library/Homebrew/cmd/install.rb
+++ Library/Homebrew/cmd/install.rb
@@ -267,7 +267,7 @@ module Homebrew
     return unless OS.linux?
     ld_so = HOMEBREW_PREFIX/"lib/ld.so"
     return if ld_so.readable?
-    sys_interpreter = ["/lib64/ld-linux-x86-64.so.2", "/lib/ld-linux.so.3", "/lib/ld-linux.so.2"].find do |s|
+    sys_interpreter = ["/lib64/ld-linux-x86-64.so.2", "/lib/ld-linux.so.3", "/lib/ld-linux.so.2", "/lib/ld-linux-armhf.so.3"].find do |s|
       Pathname.new(s).executable?
     end
     raise "Unable to locate the system's ld.so" unless sys_interpreter
