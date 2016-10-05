5f13247e4f5abfb6d1d3090efbd4c92a7c7be7ec
Shaun Jackman <sjackman@gmail.com>
2016-10-04 21:26:37 -0700

    squash-bottle-pr: Fix for Linuxbrew on Mac
    The BSD sed option -i takes a mandatory argument.

--- Library/Homebrew/dev-cmd/squash-bottle-pr.rb
+++ Library/Homebrew/dev-cmd/squash-bottle-pr.rb
@@ -13,7 +13,7 @@ module Homebrew
     marker = "Build a bottle for Linuxbrew"
     safe_system "git", "reset", "--hard", "HEAD~2"
     safe_system "git", "merge", "--squash", head
-    safe_system "sed", "-i", "-e", "/^#.*: #{marker}$/d", file
+    safe_system "sed", "-i", "", "-e", "/^#.*: #{marker}$/d", file
 
     git_editor = ENV["GIT_EDITOR"]
     ENV["GIT_EDITOR"] = "sed -n -i -e 's/.*#{marker}//p;s/^    //p'"
