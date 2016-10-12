784b71cb025bff303d27a61abe8aa709ca5587c4
Mike McQuaid <mike@mikemcquaid.com>
2016-10-12 11:18:14 +0100

    Tweak style.

--- Library/Homebrew/requirements/apr_requirement.rb
+++ Library/Homebrew/requirements/apr_requirement.rb
@@ -7,18 +7,19 @@ class AprRequirement < Requirement
   # APR shipped in Tiger is too old, but Leopard+ is usable.
   # The *-config scripts were removed in Sierra, which is widely breaking.
   satisfy(build_env: false) do
-    MacOS.version > :leopard && MacOS.version < :sierra &&
-      MacOS::CLT.installed? || Formula["apr-util"].installed?
+    next false if MacOS.version <= :leopard
+    next false if MacOS.version >= :sierra
+    MacOS::CLT.installed? || Formula["apr-util"].installed?
   end
 
   env do
-    # Prefer system Apr as much as possible, even if our's is installed.
-    unless MacOS.version > :leopard && MacOS.version < :sierra && MacOS::CLT.installed?
-      ENV.prepend_path "PATH", Formula["apr-util"].opt_bin
-      ENV.prepend_path "PATH", Formula["apr"].opt_bin
-      ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr"].opt_libexec}/lib/pkgconfig"
-      ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr-util"].opt_libexec}/lib/pkgconfig"
-    end
+    next if MacOS.version <= :leopard
+    next if MacOS.version >= :sierra
+    next if MacOS::CLT.installed?
+    ENV.prepend_path "PATH", Formula["apr-util"].opt_bin
+    ENV.prepend_path "PATH", Formula["apr"].opt_bin
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr"].opt_libexec}/lib/pkgconfig"
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["apr-util"].opt_libexec}/lib/pkgconfig"
   end
 
   def to_dependency
