f435adc2d00f4e5f91390de423f113168efe72ac
Mike McQuaid <mike@mikemcquaid.com>
2016-10-13 13:41:53 +0100

    Rename tap-readme command to tap-new.
    Write the README but also a `.travis.yml` file (and in future perhaps a
    `Jenkinsfile`).

--- Library/Homebrew/test/test_tap_new.rb
+++ Library/Homebrew/test/test_tap_new.rb
@@ -1,9 +1,8 @@
 require "helper/integration_command_test_case"
 
-class IntegrationCommandTestTapReadme < IntegrationCommandTestCase
+class IntegrationCommandTestTapNew < IntegrationCommandTestCase
   def test_tap_readme
-    assert_match "brew install homebrew/foo/<formula>",
-                 cmd("tap-readme", "foo", "--verbose")
+    assert_equal "", cmd("tap-new", "homebrew/foo", "--verbose")
     readme = HOMEBREW_LIBRARY/"Taps/homebrew/homebrew-foo/README.md"
     assert readme.exist?, "The README should be created"
   end
