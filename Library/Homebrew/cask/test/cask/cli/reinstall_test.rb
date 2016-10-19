8f8606b8c84d47da5f3bf4e7128f5e271ac212b1
Martin Schimandl <martin.schimandl@gmail.com>
2016-10-19 20:35:44 +0200

    Fix code coverage issue by introducing shutup blocks

--- Library/Homebrew/cask/test/cask/cli/reinstall_test.rb
+++ Library/Homebrew/cask/test/cask/cli/reinstall_test.rb
@@ -2,16 +2,26 @@ require "test_helper"
 
 describe Hbc::CLI::Reinstall do
   it "allows reinstalling a Cask" do
-    Hbc::CLI::Install.run("local-transmission")
+    shutup do
+      Hbc::CLI::Install.run("local-transmission")
+    end
     Hbc.load("local-transmission").must_be :installed?
-    Hbc::CLI::Reinstall.run("local-transmission")
+
+    shutup do
+      Hbc::CLI::Reinstall.run("local-transmission")
+    end
     Hbc.load("local-transmission").must_be :installed?
   end
 
   it "allows reinstalling a non installed Cask" do
-    Hbc::CLI::Uninstall.run("local-transmission")
+    shutup do
+      Hbc::CLI::Uninstall.run("local-transmission")
+    end
     Hbc.load("local-transmission").wont_be :installed?
-    Hbc::CLI::Reinstall.run("local-transmission")
+
+    shutup do
+      Hbc::CLI::Reinstall.run("local-transmission")
+    end
     Hbc.load("local-transmission").must_be :installed?
   end
 end
