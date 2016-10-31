2c798682294aaa0e5e6746a2dae831707d39a847
Robin Kunde <robin.kunde@recoursive.com>
2016-10-31 00:42:27 -0400

    Update latest clang version for 10.12

--- Library/Homebrew/os/mac/xcode.rb
+++ Library/Homebrew/os/mac/xcode.rb
@@ -195,7 +195,7 @@ module OS
         # on the older supported platform for that Xcode release, i.e there's no
         # CLT package for 10.11 that contains the Clang version from Xcode 8.
         case MacOS.version
-        when "10.12" then "800.0.38"
+        when "10.12" then "800.0.42.1"
         when "10.11" then "703.0.31"
         when "10.10" then "700.1.81"
         when "10.9"  then "600.0.57"
