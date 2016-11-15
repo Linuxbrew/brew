c20d50fc7a527fb912669c97cb3131f01aaf43f1
Josh Hagins <hagins.josh@gmail.com>
2016-11-15 11:57:10 -0500

    hbc/cmd/style: bump rubocop-cask to 0.10.6

--- Library/Homebrew/cask/lib/hbc/cli/style.rb
+++ Library/Homebrew/cask/lib/hbc/cli/style.rb
@@ -23,7 +23,7 @@ module Hbc
         $CHILD_STATUS.success?
       end
 
-      RUBOCOP_CASK_VERSION = "~> 0.10.5".freeze
+      RUBOCOP_CASK_VERSION = "~> 0.10.6".freeze
 
       def install_rubocop
         Utils.capture_stderr do
