e8d50401412b85e8d74ad206c73859940284a2fb
Mike McQuaid <mike@mikemcquaid.com>
2016-11-18 14:08:40 +0000

    testbottest: don't use TEST_FIXTURE_DIR.
    This isn't defined for formulae.

--- Library/Homebrew/test/support/fixtures/testbottest.rb
+++ Library/Homebrew/test/support/fixtures/testbottest.rb
@@ -1,7 +1,7 @@
 class Testbottest < Formula
   desc "Minimal C program and Makefile used for testing Homebrew."
   homepage "https://github.com/Homebrew/brew"
-  url "file://#{TEST_FIXTURE_DIR}/tarballs/testbottest-0.1.tbz"
+  url "file://#{File.expand_path("..", __FILE__)}/tarballs/testbottest-0.1.tbz"
   sha256 "78b54d8f31585c9773bed12b4aa4ab2ce458ebd044b9406cb24d40aa5107f082"
 
   def install
