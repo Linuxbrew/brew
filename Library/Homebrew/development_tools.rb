a3f03e2066cc83087b2179b06606aff92cc1c4f7
Mike McQuaid <mike@mikemcquaid.com>
2016-11-21 08:49:04 +0000

    development_tools: use begin/end
    A more widely used style than relying on `if`/`else` returns.

--- Library/Homebrew/development_tools.rb
+++ Library/Homebrew/development_tools.rb
@@ -42,42 +42,43 @@ class DevelopmentTools
     end
 
     def gcc_40_build_version
-      @gcc_40_build_version ||=
+      @gcc_40_build_version ||= begin
         if (path = locate("gcc-4.0")) &&
            build_version = `#{path} --version 2>/dev/null`[/build (\d{4,})/, 1]
           Version.new build_version
         else
           Version::NULL
         end
+      end
     end
     alias gcc_4_0_build_version gcc_40_build_version
 
     def gcc_42_build_version
-      @gcc_42_build_version ||=
-        begin
-          gcc = locate("gcc-4.2") || HOMEBREW_PREFIX.join("opt/apple-gcc42/bin/gcc-4.2")
-          if gcc.exist? && !gcc.realpath.basename.to_s.start_with?("llvm")&&
-             build_version = `#{gcc} --version 2>/dev/null`[/build (\d{4,})/, 1]
-            Version.new build_version
-          else
-            Version::NULL
-          end
+      @gcc_42_build_version ||= begin
+        gcc = locate("gcc-4.2") || HOMEBREW_PREFIX.join("opt/apple-gcc42/bin/gcc-4.2")
+        if gcc.exist? && !gcc.realpath.basename.to_s.start_with?("llvm")&&
+           build_version = `#{gcc} --version 2>/dev/null`[/build (\d{4,})/, 1]
+          Version.new build_version
+        else
+          Version::NULL
         end
+      end
     end
     alias gcc_build_version gcc_42_build_version
 
     def clang_version
-      @clang_version ||=
+      @clang_version ||= begin
         if (path = locate("clang")) &&
            build_version = `#{path} --version`[/(?:clang|LLVM) version (\d\.\d)/, 1]
           Version.new build_version
         else
           Version::NULL
         end
+      end
     end
 
     def clang_build_version
-      @clang_build_version ||=
+      @clang_build_version ||= begin
         if (path = locate("clang")) &&
            build_version = `#{path} --version`[/clang-(\d{2,})/, 1]
           Version.new build_version
