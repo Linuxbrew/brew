8f85eb64c4875fed514150dec755cb267a96df74
Mike McQuaid <mike@mikemcquaid.com>
2016-12-20 14:26:53 +0000

    audit: fix 32-bit check.

--- Library/Homebrew/dev-cmd/audit.rb
+++ Library/Homebrew/dev-cmd/audit.rb
@@ -448,7 +448,7 @@ class FormulaAuditor
 
   def audit_options
     formula.options.each do |o|
-      if o.name != "32-bit"
+      if o.name == "32-bit"
         problem "macOS has been 64-bit only since 10.6 so 32-bit options are deprecated."
       end
 
