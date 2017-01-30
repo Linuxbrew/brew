77401b787f96f4a50788188cdbb5d2c8fb51a852
Markus Reiter <me@reitermark.us>
2017-01-30 01:00:20 +0100

    Reorder `testing_env`.

--- Library/Homebrew/test/testing_env.rb
+++ Library/Homebrew/test/testing_env.rb
@@ -1,18 +1,18 @@
-$LOAD_PATH.unshift(File.expand_path("#{ENV["HOMEBREW_LIBRARY"]}/Homebrew"))
-$LOAD_PATH.unshift(File.expand_path("#{ENV["HOMEBREW_LIBRARY"]}/Homebrew/test/support/lib"))
-
-require "simplecov" if ENV["HOMEBREW_TESTS_COVERAGE"]
-require "global"
-
 begin
   require "minitest/autorun"
   require "minitest/reporters"
   Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(color: true)
-  require "parallel_tests/test/runtime_logger"
   require "mocha/setup"
+  require "parallel_tests/test/runtime_logger"
+  require "simplecov" if ENV["HOMEBREW_TESTS_COVERAGE"]
 rescue LoadError
-  abort "Run `bundle install` or install the mocha and minitest gems before running the tests"
+  abort "Run `bundle install` before running the tests."
 end
+
+$LOAD_PATH.unshift(File.expand_path("#{ENV["HOMEBREW_LIBRARY"]}/Homebrew"))
+$LOAD_PATH.unshift(File.expand_path("#{ENV["HOMEBREW_LIBRARY"]}/Homebrew/test/support/lib"))
+
+require "global"
 
 require "test/support/helper/test_case"
 require "test/support/helper/integration_command_test_case"
