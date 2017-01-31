e11b8b68cdf2b021dce4a11c40afddc306dab7d4
Trevor Powell <trevor.w.powell@gmail.com>
2017-01-30 22:31:09 -0600

    Update dmg.rb based on feedback
    Try "diskutil eject" twice before "diskutil unmount force".

--- Library/Homebrew/cask/lib/hbc/container/dmg.rb
+++ Library/Homebrew/cask/lib/hbc/container/dmg.rb
@@ -43,12 +43,17 @@ module Hbc
           next unless mountpath.exist?
 
           begin
-            tries ||= 2
-            @command.run("/usr/sbin/diskutil",
-                         args:         ["unmount", "force", mountpath],
-                         print_stderr: false)
-
-            raise CaskError, "Failed to unmount #{mountpath}" if mountpath.exist?
+            tries ||= 3
+            if tries > 1
+              @command.run("/usr/sbin/diskutil",
+                           args:         ["eject", mountpath],
+                           print_stderr: false)
+            else
+              @command.run("/usr/sbin/diskutil",
+                           args:         ["unmount", "force", mountpath],
+                           print_stderr: false)
+            end
+            raise CaskError, "Failed to eject #{mountpath}" if mountpath.exist?
           rescue CaskError => e
             raise e if (tries -= 1).zero?
             sleep 1
