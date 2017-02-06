ba2ec8abcc0f4a33852da4bb58af975d604569d5
Fabian Mettler <mettler.fabian@gmail.com>
2017-02-06 14:22:17 +0100

    Fix: Documentation of --only in tests cmd
    This pull request fixes the documentation for the —only flag of the
    tests cmd.

--- Library/Homebrew/dev-cmd/tests.rb
+++ Library/Homebrew/dev-cmd/tests.rb
@@ -1,4 +1,4 @@
-#:  * `tests` [`-v`] [`--coverage`] [`--generic`] [`--no-compat`] [`--only=`<test_script/test_method>] [`--seed` <seed>] [`--trace`] [`--online`] [`--official-cmd-taps`]:
+#:  * `tests` [`-v`] [`--coverage`] [`--generic`] [`--no-compat`] [`--only=`<test_script:test_method>] [`--seed` <seed>] [`--trace`] [`--online`] [`--official-cmd-taps`]:
 #:    Run Homebrew's unit and integration tests.
 
 require "fileutils"
