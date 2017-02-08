2ade29a5cf5df84ed9fb7dcf429c59c3e084a6a1
Markus Reiter <me@reitermark.us>
2017-02-08 12:01:03 +0100

    Convert Cat test to spec.

--- Library/Homebrew/cask/spec/cask/cli/cat_spec.rb
+++ Library/Homebrew/cask/spec/cask/cli/cat_spec.rb
@@ -1,9 +1,9 @@
-require "test_helper"
+require "spec_helper"
 
 describe Hbc::CLI::Cat do
   describe "given a basic Cask" do
-    before do
-      @expected_output = <<-EOS.undent
+    let(:expected_output) {
+      <<-EOS.undent
         cask 'basic-cask' do
           version '1.2.3'
           sha256 '8c62a2b791cf5f0da6066a0a4b6e85f62949cd60975da062df44adf887f4370b'
@@ -14,46 +14,46 @@ describe Hbc::CLI::Cat do
           app 'TestCask.app'
         end
       EOS
-    end
+    }
 
     it "displays the Cask file content about the specified Cask" do
-      lambda {
+      expect {
         Hbc::CLI::Cat.run("basic-cask")
-      }.must_output(@expected_output)
+      }.to output(expected_output).to_stdout
     end
 
     it "throws away additional Cask arguments and uses the first" do
-      lambda {
+      expect {
         Hbc::CLI::Cat.run("basic-cask", "local-caffeine")
-      }.must_output(@expected_output)
+      }.to output(expected_output).to_stdout
     end
 
     it "throws away stray options" do
-      lambda {
+      expect {
         Hbc::CLI::Cat.run("--notavalidoption", "basic-cask")
-      }.must_output(@expected_output)
+      }.to output(expected_output).to_stdout
     end
   end
 
   it "raises an exception when the Cask does not exist" do
-    lambda {
+    expect {
       Hbc::CLI::Cat.run("notacask")
-    }.must_raise Hbc::CaskUnavailableError
+    }.to raise_error(Hbc::CaskUnavailableError)
   end
 
   describe "when no Cask is specified" do
     it "raises an exception" do
-      lambda {
+      expect {
         Hbc::CLI::Cat.run
-      }.must_raise Hbc::CaskUnspecifiedError
+      }.to raise_error(Hbc::CaskUnspecifiedError)
     end
   end
 
   describe "when no Cask is specified, but an invalid option" do
     it "raises an exception" do
-      lambda {
+      expect {
         Hbc::CLI::Cat.run("--notavalidoption")
-      }.must_raise Hbc::CaskUnspecifiedError
+      }.to raise_error(Hbc::CaskUnspecifiedError)
     end
   end
 end
