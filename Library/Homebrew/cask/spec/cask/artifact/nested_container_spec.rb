3db8cdf8613694a26051e0ee291f931a7ca00ae3
Markus Reiter <me@reitermark.us>
2017-02-08 14:28:18 +0100

    Convert NestedContainer test to spec.

--- Library/Homebrew/cask/spec/cask/artifact/nested_container_spec.rb
+++ Library/Homebrew/cask/spec/cask/artifact/nested_container_spec.rb
@@ -1,17 +1,17 @@
-require "test_helper"
+require "spec_helper"
 
 describe Hbc::Artifact::NestedContainer do
   describe "install" do
     it "extracts the specified paths as containers" do
       cask = Hbc::CaskLoader.load_from_file(TEST_FIXTURE_DIR/"cask/Casks/nested-app.rb").tap do |c|
-        TestHelper.install_without_artifacts(c)
+        InstallHelper.install_without_artifacts(c)
       end
 
       shutup do
         Hbc::Artifact::NestedContainer.new(cask).install_phase
       end
 
-      cask.staged_path.join("MyNestedApp.app").must_be :directory?
+      expect(cask.staged_path.join("MyNestedApp.app")).to be_a_directory
     end
   end
 end
