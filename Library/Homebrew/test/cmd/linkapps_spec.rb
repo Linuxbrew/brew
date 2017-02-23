5bf70805d8c134174dfb55791c25066239d9bc0e
Markus Reiter <me@reitermark.us>
2017-02-23 06:06:26 +0100

    Convert `brew linkapps` test to spec.

---
+++ Library/Homebrew/test/cmd/linkapps_spec.rb
@@ -0,0 +1,24 @@
+describe "brew linkapps", :integration_test do
+  let(:home_dir) { @home_dir = Pathname.new(Dir.mktmpdir) }
+  let(:apps_dir) { home_dir/"Applications" }
+
+  after(:each) do
+    home_dir.rmtree unless @home_dir.nil?
+  end
+
+  it "symlinks applications" do
+    apps_dir.mkpath
+
+    setup_test_formula "testball"
+
+    source_app = HOMEBREW_CELLAR/"testball/0.1/TestBall.app"
+    source_app.mkpath
+
+    expect { brew "linkapps", "--local", "HOME" => home_dir }
+      .to output(/Linking: #{Regexp.escape(source_app)}/).to_stdout
+      .and output(/`brew linkapps` has been deprecated/).to_stderr
+      .and be_a_success
+
+    expect(apps_dir/"TestBall.app").to be_a_symlink
+  end
+end
