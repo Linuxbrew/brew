4063f4159dfa36132db4045e8ec4f7f722cdf9bb
Joshua Liu <joshliu6830@gmail.com>
2017-02-23 01:54:44 -0500

    Updated deps_spec to match style

--- Library/Homebrew/test/deps_spec.rb
+++ Library/Homebrew/test/deps_spec.rb
@@ -1,14 +1,28 @@
-describe Homebrew do
-  describe "#deps" do
+describe "brew deps", :integration_test do
+  before(:each) do
     setup_test_formula "foo"
     setup_test_formula "bar"
     setup_test_formula "baz", <<-EOS.undent
       url "https://example.com/baz-1.0"
       depends_on "bar"
     EOS
+  end
 
-    expect(cmd("deps", "foo")).to eq("")
-    expect(cmd("deps", "bar")).to eq("foo")
-    expect(cmd("deps", "baz")).to eq("bar\nfoo")
+  it "outputs nothing for formula foo" do
+    expect { brew "deps", "foo" }.to output("").to_stdout
+      .and not_to_output.to_stderr
+      .and be_a_success
+  end
+
+  it "outputs foo for formula bar" do
+    expect { brew "deps", "bar" }.to output("foo\n").to_stdout
+      .and not_to_output.to_stderr
+      .and be_a_success
+  end
+
+  it "outputs formulae bar and foo for formula baz" do
+    expect { brew "deps", "baz" }.to output("bar\nfoo\n").to_stdout
+      .and not_to_output.to_stderr
+      .and be_a_success
   end
 end
