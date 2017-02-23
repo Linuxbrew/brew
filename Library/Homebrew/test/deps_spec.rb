b2795bc90239fc94dffdc0d1995bf0e8c844f28d
Joshua Liu <joshliu6830@gmail.com>
2017-02-23 05:25:24 -0500

    Corrected deps_spec descriptions and small formatting changes

--- Library/Homebrew/test/deps_spec.rb
+++ Library/Homebrew/test/deps_spec.rb
@@ -9,19 +9,22 @@ describe "brew deps", :integration_test do
   end
 
   it "outputs no dependencies for a Formula that has no dependencies" do
-    expect { brew "deps", "foo" }.to output("").to_stdout
+    expect { brew "deps", "foo" }
+      .to output("").to_stdout
       .and not_to_output.to_stderr
       .and be_a_success
   end
 
   it "outputs a dependency for a Formula that has one dependency" do
-    expect { brew "deps", "bar" }.to output("foo\n").to_stdout
+    expect { brew "deps", "bar" }
+      .to output("foo\n").to_stdout
       .and not_to_output.to_stderr
       .and be_a_success
   end
 
-  it "outputs dependencies on separate lines for a Formula that has multiple dependencies" do
-    expect { brew "deps", "baz" }.to output("bar\nfoo\n").to_stdout
+  it "outputs all of a Formula's dependencies and their dependencies on separate lines" do
+    expect { brew "deps", "baz" }
+      .to output("bar\nfoo\n").to_stdout
       .and not_to_output.to_stderr
       .and be_a_success
   end
