7acb3d588d096b744d2bc4ca9b6743c29bdde100
Shaun Jackman <sjackman@gmail.com>
2017-02-24 13:45:22 -0800

    superenv: Add binutils to PATH for Linuxbrew

--- Library/Homebrew/extend/os/linux/extend/ENV/super.rb
+++ Library/Homebrew/extend/os/linux/extend/ENV/super.rb
@@ -4,6 +4,13 @@ module Superenv
     (HOMEBREW_SHIMS_PATH/"linux/super").realpath
   end
 
+  def homebrew_extra_paths
+    paths = []
+    binutils = Formula["binutils"]
+    paths << binutils.opt_bin if binutils.installed?
+    paths
+  end
+
   def determine_rpath_paths
     paths = ["#{HOMEBREW_PREFIX}/lib"]
     paths += run_time_deps.map { |d| d.opt_lib.to_s }
