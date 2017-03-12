b5dfd17a1ee46a544bf2a037239b9ce4a1c88950
Bob W. Hogg <rwhogg@linux.com>
2017-03-12 12:56:25 -0700

    dependency_collector_spec: Partially fix for Linuxbrew
    Some of the tests are only failing because :x11 maps to
    XorgRequirement instead of X11Requirement on non-Mac.
    
    Two of them also fail for other reasons and require
    more investigation.
    
    Signed-off-by: Bob W. Hogg <rwhogg@linux.com>

--- Library/Homebrew/test/dependency_collector_spec.rb
+++ Library/Homebrew/test/dependency_collector_spec.rb
@@ -11,6 +11,14 @@ describe DependencyCollector do
     subject.requirements.find { |req| req.is_a? klass }
   end
 
+  def x_requirement
+    if OS.mac?
+      X11Requirement
+    else
+      XorgRequirement
+    end
+  end
+
   after(:each) do
     described_class.clear_cache
   end
@@ -29,7 +37,8 @@ describe DependencyCollector do
 
     specify "requirement creation" do
       subject.add :x11
-      expect(find_requirement(X11Requirement)).to be_an_instance_of(X11Requirement)
+      req = x_requirement
+      expect(find_requirement(req)).to be_an_instance_of(req)
     end
 
     it "deduplicates requirements" do
@@ -40,13 +49,13 @@ describe DependencyCollector do
     specify "requirement tags" do
       subject.add x11: "2.5.1"
       subject.add xcode: :build
-      expect(find_requirement(X11Requirement).tags).to be_empty
+      expect(find_requirement(x_requirement).tags).to be_empty
       expect(find_requirement(XcodeRequirement)).to be_a_build_requirement
     end
 
     specify "x11 without tag" do
       subject.add :x11
-      expect(find_requirement(X11Requirement).tags).to be_empty
+      expect(find_requirement(x_requirement).tags).to be_empty
     end
 
     specify "x11 with minimum version" do
@@ -56,7 +65,7 @@ describe DependencyCollector do
 
     specify "x11 with tag" do
       subject.add x11: :optional
-      expect(find_requirement(X11Requirement)).to be_optional
+      expect(find_requirement(x_requirement)).to be_optional
     end
 
     specify "x11 with minimum version and tag" do
