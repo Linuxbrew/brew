de6650683b9ef4280a51e2c66553e376bb9e18d2
JCount <JCount42@gmail.com>
2017-03-14 17:11:39 -0400

    gpg2_requirement: indicate 2.1.x "modern" series is supported as well

--- Library/Homebrew/requirements/gpg2_requirement.rb
+++ Library/Homebrew/requirements/gpg2_requirement.rb
@@ -7,6 +7,6 @@ class GPG2Requirement < Requirement
 
   # MacGPG2/GPGTools installs GnuPG 2.0.x as a vanilla `gpg` symlink
   # pointing to `gpg2`, as do we. Ensure we're actually using a 2.0 `gpg`.
-  # Temporarily, only support 2.0.x rather than the 2.1.x "modern" series.
+  # Support both the 2.0.x "stable" and 2.1.x "modern" series.
   satisfy(build_env: false) { Gpg.gpg2 || Gpg.gpg }
 end
