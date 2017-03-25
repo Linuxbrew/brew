ca37e9646946240826ecfbb7333582e96f964c57
Raza Hussain <razahussain@Razas-MacBook-Pro.local>
2017-03-25 16:20:51 -0400

    updating the exception for "kibana" for the "@" naming scheme of its formula

--- Library/Homebrew/dev-cmd/audit.rb
+++ Library/Homebrew/dev-cmd/audit.rb
@@ -1176,7 +1176,7 @@ class FormulaAuditor
       problem "Use `assert_match` instead of `assert ...include?`"
     end
 
-    if line.include?('system "npm", "install"') && !line.include?("Language::Node") && formula.name !~ /^kibana(\d{2})?$/
+    if line.include?('system "npm", "install"') && !line.include?("Language::Node") && formula.name !~ /^kibana(\@\d{1,2}(\.\d{1,2})?)?$/
       problem "Use Language::Node for npm install args"
     end
 
