755d43d46dc20b9e646909e08de482141de83777
William Roe <git@wjlr.org.uk>
2017-03-27 11:27:48 +0100

    Add test for verbose brew outdated output
    Split the tests up into quiet and verbose output with contexts.

--- Library/Homebrew/test/cmd/outdated_spec.rb
+++ Library/Homebrew/test/cmd/outdated_spec.rb
@@ -1,11 +1,25 @@
 describe "brew outdated", :integration_test do
-  it "prints outdated Formulae" do
-    setup_test_formula "testball"
-    (HOMEBREW_CELLAR/"testball/0.0.1/foo").mkpath
+  context "quiet output" do
+    it "prints outdated Formulae" do
+      setup_test_formula "testball"
+      (HOMEBREW_CELLAR/"testball/0.0.1/foo").mkpath
 
-    expect { brew "outdated" }
-      .to output("testball\n").to_stdout
-      .and not_to_output.to_stderr
-      .and be_a_success
+      expect { brew "outdated" }
+        .to output("testball\n").to_stdout
+        .and not_to_output.to_stderr
+        .and be_a_success
+    end
+  end
+
+  context "verbose output" do
+    it "prints out the installed and newer versions" do
+      setup_test_formula "testball"
+      (HOMEBREW_CELLAR/"testball/0.0.1/foo").mkpath
+
+      expect { brew "outdated", "--verbose" }
+        .to output("testball (0.0.1) < 0.1\n").to_stdout
+        .and not_to_output.to_stderr
+        .and be_a_success
+    end
   end
 end
