43b64bfed1d22ca7797e9419eed06570d47d8f26
JCount <JCount42@gmail.com>
2017-04-07 16:26:19 -0400

    gpg_spec: use new version method to directly compare version objects

--- Library/Homebrew/test/gpg_spec.rb
+++ Library/Homebrew/test/gpg_spec.rb
@@ -12,14 +12,12 @@ describe Gpg do
 
         shutup do
           subject.create_test_key(dir)
-          gpg = subject::GPG_EXECUTABLE
-          @version = Utils.popen_read(gpg, "--version")[/\d\.\d/, 0]
         end
 
-        if @version.to_s.start_with?("2.1")
-          expect(dir/".gnupg/pubring.kbx").to be_file
+        if subject.version == Version.create("2.0")
+          expect(dir/".gnupg/secring.gpg").to be_a_file
         else
-          expect(dir/".gnupg/secring.gpg").to be_file
+          expect(dir/".gnupg/pubring.kbx").to be_a_file
         end
       end
     end
