d251be1eeb84022a28193d03a4f6ebcfaa1ed9ee
Markus Reiter <me@reitermark.us>
2017-04-20 04:47:03 +0200

    Ensure `readline_nonblock` returns an UTF-8 string.

--- Library/Homebrew/extend/io.rb
+++ Library/Homebrew/extend/io.rb
@@ -1,10 +1,17 @@
 class IO
   def readline_nonblock(sep = $INPUT_RECORD_SEPARATOR)
+    line = ""
     buffer = ""
-    buffer.concat(read_nonblock(1)) while buffer[-1] != sep
-    buffer
+
+    loop do
+      break if buffer == sep
+      read_nonblock(1, buffer)
+      line.concat(buffer)
+    end
+
+    line
   rescue IO::WaitReadable, EOFError => e
-    raise e if buffer.empty?
-    buffer
+    raise e if line.empty?
+    line
   end
 end
