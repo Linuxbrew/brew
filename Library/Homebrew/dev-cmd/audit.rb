9aeb8f0f0b131919b187f4835a057e4fa4afca82
Dominyk Tiller <dominyktiller@gmail.com>
2017-05-02 19:30:41 +0100

    audit: match start of string, not line

--- Library/Homebrew/dev-cmd/audit.rb
+++ Library/Homebrew/dev-cmd/audit.rb
@@ -511,7 +511,7 @@ class FormulaAuditor
     reason.sub!(name, "")
     first_word = reason.split[0]
 
-    if reason =~ /^[A-Z]/ && !reason.start_with?(*whitelist)
+    if reason =~ /\A[A-Z]/ && !reason.start_with?(*whitelist)
       problem <<-EOS.undent
         '#{first_word}' from the keg_only reason should be '#{first_word.downcase}'.
       EOS
