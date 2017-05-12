9b1afa50bee40cc5ecd77e91aecc3682ea0555cb
Shaun Jackman <sjackman@gmail.com>
2017-05-12 15:13:33 -0700

    Add ENV.libxml2 for Linuxbrew

--- Library/Homebrew/extend/os/linux/extend/ENV/std.rb
+++ Library/Homebrew/extend/os/linux/extend/ENV/std.rb
@@ -1,4 +1,11 @@
 module Stdenv
   # needed by XorgRequirement
   def x11; end
+
+  # Add libxml2 to CPPFLAGS
+  def libxml2
+    append "CPPFLAGS", "-I#{Formula["libxml2"].include/"libxml2"}"
+  rescue FormulaUnavailableError
+    nil
+  end
 end
