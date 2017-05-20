811f4c5f237dad654c3eba81dfa96b7e9f29c10f
Markus Reiter <me@reitermark.us>
2017-05-20 04:08:59 +0200

    Refactor `CLI::Zap`.

--- Library/Homebrew/cask/lib/hbc/cli/zap.rb
+++ Library/Homebrew/cask/lib/hbc/cli/zap.rb
@@ -2,9 +2,16 @@ module Hbc
   class CLI
     class Zap < Base
       def self.run(*args)
-        cask_tokens = cask_tokens_from(args)
-        raise CaskUnspecifiedError if cask_tokens.empty?
-        cask_tokens.each do |cask_token|
+        new(*args).run
+      end
+
+      def initialize(*args)
+        @cask_tokens = self.class.cask_tokens_from(args)
+        raise CaskUnspecifiedError if @cask_tokens.empty?
+      end
+
+      def run
+        @cask_tokens.each do |cask_token|
           odebug "Zapping Cask #{cask_token}"
           cask = CaskLoader.load(cask_token)
           Installer.new(cask).zap
