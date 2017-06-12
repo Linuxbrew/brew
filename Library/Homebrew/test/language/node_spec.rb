cf5db0e0df38aee9e26099f6e4e8331d1f9352d5
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-06-13 02:29:16 +0530

    Fixed style errors

--- Library/Homebrew/test/language/node_spec.rb
+++ Library/Homebrew/test/language/node_spec.rb
@@ -2,25 +2,25 @@ require "language/node"
 
 describe Language::Node do
   specify "#npm_cache_config" do
-  	shutup do
-  		ret_val = described_class.npm_cache_config
-  		expect(ret_val).to eq("cache=#{HOMEBREW_CACHE}/npm_cache\n")
-  	end
+    shutup do
+      ret_val = described_class.npm_cache_config
+      expect(ret_val).to eq("cache=#{HOMEBREW_CACHE}/npm_cache\n")
+    end
   end
 
   describe "#pack_for_installation" do
-  	it "raises error with non zero exitstatus" do
+    it "raises error with non zero exitstatus" do
       shutup do
-  			expect{described_class.pack_for_installation}.to raise_error
-  		end
-  	end
+        expect { described_class.pack_for_installation }.to raise_error
+      end
+    end
 
-  	it "does not raise error with a zero exitstatus" do
-  		shutup do
-  			allow_any_instance_of(Process::Status).to receive(:exitstatus).and_return(0)
-  			expect{described_class.pack_for_installation}.not_to raise_error
-  		end	
-  	end
+    it "does not raise error with a zero exitstatus" do
+      shutup do
+        allow_any_instance_of(Process::Status).to receive(:exitstatus).and_return(0)
+        expect { described_class.pack_for_installation }.not_to raise_error
+      end
+    end
   end
 
   describe "#setup_npm_environment" do
@@ -33,7 +33,7 @@ describe Language::Node do
     it "npmrc does not exist" do
       shutup do
         allow_any_instance_of(Pathname).to receive(:exist?).and_return(false)
-        described_class.setup_npm_environment 
+        described_class.setup_npm_environment
       end
     end
   end
@@ -53,5 +53,4 @@ describe Language::Node do
       expect(resp).to eq(["--verbose"])
     end
   end
-
 end
