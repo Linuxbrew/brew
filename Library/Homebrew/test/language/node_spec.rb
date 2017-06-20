5fed1f5cbeca11d54fd69070c2e628e165e8d80d
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-06-21 03:03:52 +0530

    Attempt to fix tests

--- Library/Homebrew/test/language/node_spec.rb
+++ Library/Homebrew/test/language/node_spec.rb
@@ -31,7 +31,7 @@ describe Language::Node do
 
     it "does not raise error with a zero exitstatus" do
       allow(Utils).to receive(:popen_read).with("npm pack").and_return("pack")
-      allow_any_instance_of(Process::Status).to receive(:exitstatus).and_return(0)
+      allow($?).to receive(:exitstatus).and_return(0)
       resp = subject.std_npm_install_args(npm_install_arg)
       expect(resp).to include("--prefix=#{npm_install_arg}", "#{Dir.pwd}/pack")
     end
