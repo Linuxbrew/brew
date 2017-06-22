bfd19f91708abf0564a482377c5107b30b934437
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-06-23 03:27:21 +0530

    Stubbed nil:NilClass

--- Library/Homebrew/test/language/node_spec.rb
+++ Library/Homebrew/test/language/node_spec.rb
@@ -31,7 +31,8 @@ describe Language::Node do
 
     it "does not raise error with a zero exitstatus" do
       allow(Utils).to receive(:popen_read).with("npm pack").and_return("pack")
-      allow($?).to receive(:exitstatus).and_return(0)
+      allow_any_instance_of(Process::Status).to receive(:exitstatus).and_return(0)
+      allow_any_instance_of(nil::NilClass).to receive(:exitstatus).and_return(0)
       resp = subject.std_npm_install_args(npm_install_arg)
       expect(resp).to include("--prefix=#{npm_install_arg}", "#{Dir.pwd}/pack")
     end
