fac708da7a436996535b4c3cfd1a321267ff6d08
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-06-25 06:27:53 +0530

    changes test names for class methods

--- Library/Homebrew/test/language/java_spec.rb
+++ Library/Homebrew/test/language/java_spec.rb
@@ -1,7 +1,7 @@
 require "language/java"
 
 describe Language::Java do
-  describe "#self.java_home_env" do
+  describe "::java_home_env" do
     it "returns java_home path with version if version specified" do
       java_home = described_class.java_home_env("blah")
       expect(java_home[:JAVA_HOME]).to include("--version blah")
@@ -13,16 +13,15 @@ describe Language::Java do
     end
   end
 
-  describe "#self.overridable_java_home_env" do
-    ENV["JAVA_HOME"] = "java_home"
+  describe "::overridable_java_home_env" do
     it "returns java_home path with version if version specified" do
       java_home = described_class.overridable_java_home_env("blah")
-      expect(java_home[:JAVA_HOME]).to eq("java_home")
+      expect(java_home[:JAVA_HOME]).to include("--version blah")
     end
 
     it "returns java_home path without version if version is not specified" do
       java_home = described_class.overridable_java_home_env
-      expect(java_home[:JAVA_HOME]).to eq("java_home")
+      expect(java_home[:JAVA_HOME]).not_to include("--version")
     end
   end
 end
