1df9a475a8ebad1c15bb7f81a6ce70e4940255a6
mansimarkaur <mansimarkaur.mks@gmail.com>
2017-06-25 06:04:09 +0530

    Set java environment var to test overriding

--- Library/Homebrew/test/language/java_spec.rb
+++ Library/Homebrew/test/language/java_spec.rb
@@ -14,15 +14,15 @@ describe Language::Java do
   end
 
   describe "#self.overridable_java_home_env" do
-    let(:JAVA_HOME) { "haha" }
+    ENV["JAVA_HOME"] = "java_home"
     it "returns java_home path with version if version specified" do
       java_home = described_class.overridable_java_home_env("blah")
-      expect(java_home[:JAVA_HOME]).to eq("haha")
+      expect(java_home[:JAVA_HOME]).to eq("java_home")
     end
 
     it "returns java_home path without version if version is not specified" do
       java_home = described_class.overridable_java_home_env
-      expect(java_home[:JAVA_HOME]).not_to include("--version")
+      expect(java_home[:JAVA_HOME]).to eq("java_home")
     end
   end
 end
