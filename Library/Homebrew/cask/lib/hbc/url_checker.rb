2703646b1008e8fc0a922e4677d9b2aadcea4b4d
Markus Reiter <me@reitermark.us>
2017-06-27 11:55:23 +0200

    Remove `utf8_inspect` method.

--- Library/Homebrew/cask/lib/hbc/url_checker.rb
+++ Library/Homebrew/cask/lib/hbc/url_checker.rb
@@ -38,7 +38,7 @@ module Hbc
     def _check_response_status
       ok = OK_RESPONSES[cask.url.scheme]
       return if ok.include?(@response_status)
-      add_error "unexpected http response, expecting #{ok.map(&:utf8_inspect).join(" or ")}, got #{@response_status.utf8_inspect}"
+      add_error "unexpected http response, expecting #{ok.map(&:to_s).join(" or ")}, got #{@response_status}"
     end
 
     def _get_data_from_request
