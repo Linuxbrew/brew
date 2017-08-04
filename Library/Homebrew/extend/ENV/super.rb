0949ed7e9da0c8e86c4c216216e63b041ee1ea8b
Shaun Jackman <sjackman@gmail.com>
2017-08-04 14:15:44 -0700

    ENV: Add formula.lib to RPATH for unlinked kegs
    Unlinked kegs, such as versioned kegs, need their lib directory in their RPATH.

--- Library/Homebrew/extend/ENV/super.rb
+++ Library/Homebrew/extend/ENV/super.rb
@@ -62,7 +62,7 @@ module Superenv
     self["HOMEBREW_ISYSTEM_PATHS"] = determine_isystem_paths
     self["HOMEBREW_INCLUDE_PATHS"] = determine_include_paths
     self["HOMEBREW_LIBRARY_PATHS"] = determine_library_paths
-    self["HOMEBREW_RPATH_PATHS"] = determine_rpath_paths
+    self["HOMEBREW_RPATH_PATHS"] = determine_rpath_paths(formula)
     self["HOMEBREW_DYNAMIC_LINKER"] = determine_dynamic_linker_path(formula)
     self["HOMEBREW_DEPENDENCIES"] = determine_dependencies
     self["HOMEBREW_FORMULA_PREFIX"] = formula.prefix unless formula.nil?
@@ -189,8 +189,11 @@ module Superenv
     []
   end
 
-  def determine_rpath_paths
-    PATH.new(determine_extra_rpath_paths).existing
+  def determine_rpath_paths(formula)
+    PATH.new(
+      (formula.lib unless formula.nil?),
+      PATH.new(determine_extra_rpath_paths).existing,
+    )
   end
 
   def determine_dynamic_linker_path(_formula)
