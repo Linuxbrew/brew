0762cc6e74446bc46fa2daa4faffa229dd40e49c
Dominyk Tiller <dominyktiller@gmail.com>
2017-08-09 17:02:14 +0100

    gpg2_requirement: flip search priority

--- Library/Homebrew/requirements/gpg2_requirement.rb
+++ Library/Homebrew/requirements/gpg2_requirement.rb
@@ -5,8 +5,8 @@ class GPG2Requirement < Requirement
   fatal true
   default_formula "gnupg"
 
-  # MacGPG2/GPGTools installs GnuPG 2.0.x as a vanilla `gpg` symlink
-  # pointing to `gpg2`, as do we. Ensure we're actually using a 2.0 `gpg`.
-  # Support both the 2.0.x "stable" and 2.1.x "modern" series.
-  satisfy(build_env: false) { Gpg.gpg2 || Gpg.gpg }
+  # GPGTools installs GnuPG 2.0.x as a vanilla `gpg` symlink
+  # pointing to `gpg2`. Homebrew install 2.1.x as a non-symlink `gpg`.
+  # We support both the 2.0.x "stable" and 2.1.x "modern" series here.
+  satisfy(build_env: false) { Gpg.gpg || Gpg.gpg2 }
 end
