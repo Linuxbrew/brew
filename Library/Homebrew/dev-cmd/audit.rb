28c78384501e159ecb45e0ffa29ff419a22701d8
ilovezfs <ilovezfs@icloud.com>
2017-09-24 00:29:34 -0700

    audit: also skip homebrew/science duplicates found by search_taps
    Follow-up to #3202.

--- Library/Homebrew/dev-cmd/audit.rb
+++ Library/Homebrew/dev-cmd/audit.rb
@@ -430,6 +430,7 @@ class FormulaAuditor
 
     if @online
       Homebrew.search_taps(name, silent: true).each do |tap_formula_full_name|
+        next if tap_formula_full_name.start_with?("homebrew/science/")
         tap_formula_name = tap_formula_full_name.split("/").last
         next if tap_formula_name != name
         same_name_tap_formulae << tap_formula_full_name
