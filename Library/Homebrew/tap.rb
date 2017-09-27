fe5c885da0b8b69a5de74647e4181b137acb835f
Ben Muschol <benmuschol@gmail.com>
2017-09-27 16:32:13 -0400

    Implement changes

--- Library/Homebrew/tap.rb
+++ Library/Homebrew/tap.rb
@@ -118,6 +118,12 @@ class Tap
     path.git?
   end
 
+  # git branch for this {Tap}.
+  def git_branch
+    raise TapUnavailableError, name unless installed?
+    path.git_branch
+  end
+
   # git HEAD for this {Tap}.
   def git_head
     raise TapUnavailableError, name unless installed?
