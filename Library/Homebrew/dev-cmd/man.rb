e7643ead4b1669e384aa41e53a31fa28795ed955
Maxim Belkin <maxim.belkin@gmail.com>
2017-10-02 17:03:14 -0500

    Fix the whitespaces
    :(

--- Library/Homebrew/dev-cmd/man.rb
+++ Library/Homebrew/dev-cmd/man.rb
@@ -69,8 +69,7 @@ module Homebrew
                                         .gsub(/\[([^\]]+)\]\([^)]+\)/, '\1')
     variables[:maintainers] = readme.read[/(Linuxbrew's other current maintainers are .*\.)/, 1]
                                     .gsub(/\[([^\]]+)\]\([^)]+\)/, '\1')
-
-	variables[:former_maintainers] = readme.read[/(Former maintainers .*\.)/, 1]
+    variables[:former_maintainers] = readme.read[/(Former maintainers .*\.)/, 1]
                                            .gsub(/\[([^\]]+)\]\([^)]+\)/, '\1')
 
     ERB.new(template, nil, ">").result(variables.instance_eval { binding })
