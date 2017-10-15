487bec957007612956ff97b3912f88768becaa32
Markus Reiter <me@reitermark.us>
2017-10-15 02:38:10 +0200

    Move `String#undent` to `compat`.

--- Library/Homebrew/test/string_spec.rb
+++ Library/Homebrew/test/string_spec.rb
@@ -1,41 +1,5 @@
 require "extend/string"
 
-describe String do
-  describe "#undent" do
-    it "removes leading whitespace, taking the first line as reference" do
-      string = <<-EOS.unindent
-                hi
-                ........my friend over
-                  there
-      EOS
-
-      expect(string).to eq("hi\n........my friend over\n  there\n")
-    end
-
-    it "removes nothing if the text is not indented" do
-      string = <<-EOS.unindent
-        hi
-        I'm not indented
-      EOS
-
-      expect(string).to eq("hi\nI'm not indented\n")
-    end
-
-    it "can be nested" do
-      nested_string = <<~EOS
-        goodbye
-      EOS
-
-      string = <<~EOS
-        hello
-        #{nested_string}
-      EOS
-
-      expect(string).to eq("hello\ngoodbye\n\n")
-    end
-  end
-end
-
 describe StringInreplaceExtension do
   subject { string.extend(described_class) }
   let(:string) { "foobar" }
