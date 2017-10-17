d601edaf57ea7968873e3ae9452cec68826d814c
Mike McQuaid <mike@mikemcquaid.com>
2017-10-17 20:51:32 +0100

    vendor-install: use full shasum PATH.
    Otherwise things can explode if there's a random `shasum`.
    
    See #3281.

--- Library/Homebrew/cmd/vendor-install.sh
+++ Library/Homebrew/cmd/vendor-install.sh
@@ -81,9 +81,9 @@ fetch() {
     trap - SIGINT
   fi
 
-  if [[ -x "$(which shasum)" ]]
+  if [[ -x "/usr/bin/shasum" ]]
   then
-    sha="$(shasum -a 256 "$CACHED_LOCATION" | cut -d' ' -f1)"
+    sha="$(/usr/bin/shasum -a 256 "$CACHED_LOCATION" | cut -d' ' -f1)"
   elif [[ -x "$(which sha256sum)" ]]
   then
     sha="$(sha256sum "$CACHED_LOCATION" | cut -d' ' -f1)"
