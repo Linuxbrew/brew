fd29511bae8ba381c72bbdc7effd842fc46b0fac
Markus Reiter <me@reitermark.us>
2017-10-21 01:57:00 +0200

    Update to RuboCop 0.51.

--- Library/Homebrew/constants.rb
+++ Library/Homebrew/constants.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 # RuboCop version used for `brew style` and `brew cask style`
-HOMEBREW_RUBOCOP_VERSION = "0.50.0"
+HOMEBREW_RUBOCOP_VERSION = "0.51.0"
 HOMEBREW_RUBOCOP_CASK_VERSION = "~> 0.14.4" # has to be updated when RuboCop version changes
