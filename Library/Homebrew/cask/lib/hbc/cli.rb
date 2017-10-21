a8ee54a35fb976ad142b1a21a718d43351ea6268
L. E. Segovia <amyspark@users.noreply.github.com>
2017-10-21 15:43:20 -0300

    Actually check that the command name was set before adding it to args

--- Library/Homebrew/cask/lib/hbc/cli.rb
+++ Library/Homebrew/cask/lib/hbc/cli.rb
@@ -154,7 +154,7 @@ module Hbc
     def run
       command_name, *args = detect_command_and_arguments(*@args)
       command = if help?
-        args.unshift(command_name) unless command_name.nil
+        args.unshift(command_name) unless command_name.nil?
         "help"
       else
         self.class.lookup_command(command_name)
