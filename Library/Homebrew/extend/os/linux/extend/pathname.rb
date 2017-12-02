e5847b652f93744d41f45b5636e67bb2748d67c7
Shaun Jackman <sjackman@gmail.com>
2017-12-01 16:29:50 -0800

    extend/pathname: Add os/linux/elf.rb

--- Library/Homebrew/extend/os/linux/extend/pathname.rb
+++ Library/Homebrew/extend/os/linux/extend/pathname.rb
@@ -1,19 +1,5 @@
-class Pathname
-  # @private
-  def elf?
-    # See: https://en.wikipedia.org/wiki/Executable_and_Linkable_Format#File_header
-    read(4) == "\x7fELF"
-  end
+require "os/linux/elf"
 
-  # @private
-  def dynamic_elf?
-    if which "readelf"
-      popen_read("readelf", "-l", to_path).include?(" DYNAMIC ")
-    elsif which "file"
-      !popen_read("file", "-L", "-b", to_path)[/dynamic|shared/].nil?
-    else
-      raise StandardError, "Neither `readelf` nor `file` is available "\
-        "to determine whether '#{self}' is dynamically or statically linked."
-    end
-  end
+class Pathname
+  prepend ELFShim
 end
