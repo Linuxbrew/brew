1acba3cef017dbc1f3537c6a45e8fe48af3a0ac0
Shaun Jackman <sjackman@gmail.com>
2017-12-07 08:44:20 -0800

    Implement X11Requirement for Linux
    Move the XQuartz implementation to extend/os/mac/requirements.

--- Library/Homebrew/extend/os/requirements/x11_requirement.rb
+++ Library/Homebrew/extend/os/requirements/x11_requirement.rb
@@ -1,1 +1,5 @@
-require "extend/os/linux/requirements/x11_requirement" if OS.linux?
+if OS.mac?
+  require "extend/os/mac/requirements/x11_requirement"
+elsif OS.linux?
+  require "extend/os/linux/requirements/x11_requirement"
+end
