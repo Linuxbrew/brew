c6dac68d8bdb652152d89a097a9ab9f270832d68
Maxim Belkin <maxim.belkin@gmail.com>
2018-02-19 10:21:01 +0000

    Code refactoring v3.0

--- Library/Homebrew/test/os/dependency_collector_spec.rb
+++ Library/Homebrew/test/os/dependency_collector_spec.rb
@@ -1,1 +1,23 @@
-require "test/dependency_collector"
+require "dependency_collector"
+
+describe DependencyCollector do
+  alias_matcher :be_a_build_requirement, :be_build
+
+  after(:each) do
+    described_class.clear_cache
+  end
+
+  describe "#add" do
+    it "creates a resource dependency from a '.zip' URL" do
+      resource = Resource.new
+      resource.url("http://example.com/foo.zip")
+      expect(subject.add(resource)).to eq(Dependency.new("zip", [:build]))
+    end
+
+    it "creates a resource dependency from a '.bz2' URL" do
+      resource = Resource.new
+      resource.url("http://example.com/foo.tar.bz2")
+      expect(subject.add(resource)).to eq(Dependency.new("bzip2", [:build]))
+    end
+  end
+end
