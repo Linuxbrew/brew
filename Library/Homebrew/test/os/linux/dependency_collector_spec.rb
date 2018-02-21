c83dd0d04b857161b47a43b54ba56c7296ac50c0
Maxim Belkin <maxim.belkin@gmail.com>
2018-02-21 13:44:51 +0000

    brew style: replace tabs with spaces

--- Library/Homebrew/test/os/linux/dependency_collector_spec.rb
+++ Library/Homebrew/test/os/linux/dependency_collector_spec.rb
@@ -12,41 +12,41 @@ describe DependencyCollector do
 
     context "when xz, zip, and bzip2 are not available" do
       it "creates a resource dependency from a '.xz' URL" do
-	resource.url("http://example.com/foo.xz")
-	allow_any_instance_of(Object).to receive(:which).with("xz")
-	expect(subject.add(resource)).to eq(Dependency.new("xz", [:build]))
+        resource.url("http://example.com/foo.xz")
+        allow_any_instance_of(Object).to receive(:which).with("xz")
+        expect(subject.add(resource)).to eq(Dependency.new("xz", [:build]))
       end
 
       it "creates a resource dependency from a '.zip' URL" do
-	resource.url("http://example.com/foo.zip")
-	allow_any_instance_of(Object).to receive(:which).with("zip")
-	expect(subject.add(resource)).to eq(Dependency.new("zip", [:build]))
+        resource.url("http://example.com/foo.zip")
+        allow_any_instance_of(Object).to receive(:which).with("zip")
+        expect(subject.add(resource)).to eq(Dependency.new("zip", [:build]))
       end
 
       it "creates a resource dependency from a '.bz2' URL" do
-	resource.url("http://example.com/foo.tar.bz2")
-	allow_any_instance_of(Object).to receive(:which).with("bzip2")
-	expect(subject.add(resource)).to eq(Dependency.new("bzip2", [:build]))
+        resource.url("http://example.com/foo.tar.bz2")
+        allow_any_instance_of(Object).to receive(:which).with("bzip2")
+        expect(subject.add(resource)).to eq(Dependency.new("bzip2", [:build]))
       end
     end
 
     context "when xz, zip, and bzip2 are available" do
       it "does not create a resource dependency from a '.xz' URL" do
-	resource.url("http://example.com/foo.xz")
-	allow_any_instance_of(Object).to receive(:which).with("xz").and_return(Pathname.new("foo"))
-	expect(subject.add(resource)).to be nil
+        resource.url("http://example.com/foo.xz")
+        allow_any_instance_of(Object).to receive(:which).with("xz").and_return(Pathname.new("foo"))
+        expect(subject.add(resource)).to be nil
       end
 
       it "does not create a resource dependency from a '.zip' URL" do
-	resource.url("http://example.com/foo.zip")
-	allow_any_instance_of(Object).to receive(:which).with("zip").and_return(Pathname.new("foo"))
-	expect(subject.add(resource)).to be nil
+        resource.url("http://example.com/foo.zip")
+        allow_any_instance_of(Object).to receive(:which).with("zip").and_return(Pathname.new("foo"))
+        expect(subject.add(resource)).to be nil
       end
 
       it "does not create a resource dependency from a '.bz2' URL" do
-	resource.url("http://example.com/foo.tar.bz2")
-	allow_any_instance_of(Object).to receive(:which).with("bzip2").and_return(Pathname.new("foo"))
-	expect(subject.add(resource)).to be nil
+        resource.url("http://example.com/foo.tar.bz2")
+        allow_any_instance_of(Object).to receive(:which).with("bzip2").and_return(Pathname.new("foo"))
+        expect(subject.add(resource)).to be nil
       end
     end
   end
