ccf13f08233dfe1d228dc4f12dcf249a799fcdf6
Shaun Jackman <sjackman@gmail.com>
2018-03-02 14:29:15 -0800

    DependencyCollector: Add java_dep_if_needed [Linux] (#629)
    Use the jdk formula if it's installed.
    Use the host's Java if it's sufficient.
    Otherwise install the jdk formula.

--- Library/Homebrew/extend/os/dependency_collector.rb
+++ Library/Homebrew/extend/os/dependency_collector.rb
@@ -1,2 +1,6 @@
 require "dependency_collector"
-require "extend/os/mac/dependency_collector" if OS.mac?
+if OS.mac?
+  require "extend/os/mac/dependency_collector"
+elsif OS.linux?
+  require "extend/os/linux/dependency_collector"
+end
