8860402f8fcb81e9f9630fd4fac19da7c7703f52
Mike McQuaid <mike@mikemcquaid.com>
2018-03-08 08:22:47 +0000

    utils: only try and replace tap or replaced methods.

--- Library/Homebrew/utils.rb
+++ Library/Homebrew/utils.rb
@@ -109,11 +109,7 @@ def odeprecated(method, replacement = nil, disable: false, disable_on: nil, call
 
   if ARGV.homebrew_developer? || disable ||
      Homebrew.raise_deprecation_exceptions?
-    if caller_message.match?(HOMEBREW_LIBRARY_PATH/"cmd") ||
-       caller_message.match?(HOMEBREW_LIBRARY_PATH/"dev-cmd") &&
-       replacement_message == "There is no replacement."
-      developer_message = message
-    else
+    if replacement || tap
       developer_message = message + "Or, even better, submit a PR to fix it!"
     end
     raise MethodDeprecatedError, developer_message
