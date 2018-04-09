3016658a0896548d24f5ede135285423ed0c3e13
Mike McQuaid <mike@mikemcquaid.com>
2018-04-09 08:09:18 -0700

    compat/formula: remove patches.
    This method was missed in #3896.

--- Library/Homebrew/compat/formula.rb
+++ Library/Homebrew/compat/formula.rb
@@ -1,10 +1,4 @@
 class Formula
-  def patches
-    # Don't print deprecation warning because this method is inherited
-    # when used.
-    {}
-  end
-
   def rake(*)
     odisabled "FileUtils#rake", "system \"rake\""
   end
