ba5692f7e552f3b32963df178fafcbf0cc56a7e1
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2018-05-17 13:07:56 -0400

    Only update the cache for a formula if the cache is not `empty?`.

--- Library/Homebrew/formula_installer.rb
+++ Library/Homebrew/formula_installer.rb
@@ -612,7 +612,7 @@ class FormulaInstaller
 
     # Updates the cache for a particular formula after doing an install
     DatabaseCache.new(:linkage) do |database_cache|
-      LinkageChecker.new(keg, database_cache, false, formula)
+      LinkageChecker.new(keg, database_cache, false, formula) unless database_cache.empty?
     end
 
     # let's reset Utils.git_available? if we just installed git
