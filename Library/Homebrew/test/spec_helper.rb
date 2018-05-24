0408b78a23092fb345bee7af3dcac7c62c175848
Mike McQuaid <mike@mikemcquaid.com>
2018-05-24 15:09:41 +0100

    tests: remove linkage cache env.
    Otherwise this will mess with `brew tests`.

--- Library/Homebrew/test/spec_helper.rb
+++ Library/Homebrew/test/spec_helper.rb
@@ -43,8 +43,8 @@ RSpec.configure do |config|
 
   config.filter_run_when_matching :focus
 
-  # TODO when https://github.com/rspec/rspec-expectations/pull/1056 makes
-  # it into a stable release:
+  # TODO: when https://github.com/rspec/rspec-expectations/pull/1056
+  #       makes it into a stable release:
   # config.expect_with :rspec do |c|
   #   c.max_formatted_output_length = 200
   # end
