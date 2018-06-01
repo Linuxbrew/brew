1a2c2f2e1bd4e1eff2e7cf463dc54a1d81198c52
Mike McQuaid <mike@mikemcquaid.com>
2018-06-01 13:26:45 +0100

    linkage_cache: cache input data and not results.
    Cache all the non-weak dynamic library links for a keg rather than the
    result of running `brew linkage`. This means that we correctly handle
    changes to e.g. what non-keg files are present on disk.

--- Library/Homebrew/linkage_cache_store.rb
+++ Library/Homebrew/linkage_cache_store.rb
@@ -18,106 +18,55 @@ class LinkageCacheStore < CacheStore
   #
   # @return [Boolean]
   def keg_exists?
-    !database.get(keg_name).nil?
+    !database.get(@keg_name).nil?
   end
 
   # Inserts dylib-related information into the cache if it does not exist or
   # updates data into the linkage cache if it does exist
   #
-  # @param  [Hash] array_values: hash containing KVPs of { :type => Array | Set }
   # @param  [Hash] hash_values:  hash containing KVPs of { :type => Hash }
-  # @param  [Array[Hash]] values
-  # @raise  [TypeError] error if the values are not `Arary`, `Set`, or `Hash`
   # @return [nil]
-  def update!(array_values: {}, hash_values: {}, **values)
-    values.each do |key, value|
-      if value.is_a?(Hash)
-        hash_values[key] = value
-      elsif value.is_a?(Array) || value.is_a?(Set)
-        array_values[key] = value
-      else
-        raise TypeError, <<~EOS
-          Can't store types that are not `Array`, `Set` or `Hash` in the
-          linkage store.
-        EOS
-      end
+  def update!(hash_values)
+    hash_values.each_key do |type|
+      next if HASH_LINKAGE_TYPES.include?(type)
+
+      raise TypeError, <<~EOS
+        Can't update types that are not defined for the linkage store
+      EOS
     end
 
-    database.set keg_name, ruby_hash_to_json_string(
-      array_values: format_array_values(array_values),
-      hash_values: format_hash_values(hash_values),
-    )
+    database.set @keg_name, ruby_hash_to_json_string(hash_values)
   end
 
   # @param  [Symbol] the type to fetch from the `LinkageCacheStore`
-  # @raise  [TypeError] error if the type is not in `HASH_LINKAGE_TYPES` or
-  #   `ARRAY_LINKAGE_TYPES`
-  # @return [Hash | Array]
+  # @raise  [TypeError] error if the type is not in `HASH_LINKAGE_TYPES`
+  # @return [Hash]
   def fetch_type(type)
-    if HASH_LINKAGE_TYPES.include?(type)
-      fetch_hash_values(type)
-    elsif ARRAY_LINKAGE_TYPES.include?(type)
-      fetch_array_values(type)
-    else
+    unless HASH_LINKAGE_TYPES.include?(type)
       raise TypeError, <<~EOS
         Can't fetch types that are not defined for the linkage store
       EOS
     end
+
+    return {} unless keg_exists?
+
+    fetch_hash_values(type)
   end
 
   # @return [nil]
   def flush_cache!
-    database.delete(keg_name)
+    database.delete(@keg_name)
   end
 
   private
 
-  ARRAY_LINKAGE_TYPES = [
-    :system_dylibs, :variable_dylibs, :broken_dylibs, :indirect_deps,
-    :undeclared_deps, :unnecessary_deps
-  ].freeze
-  HASH_LINKAGE_TYPES = [:brewed_dylibs, :reverse_links, :broken_deps].freeze
-
-  # @return [String] the key to lookup items in the `CacheStore`
-  attr_reader :keg_name
-
-  # @param  [Symbol] the type to fetch from the `LinkageCacheStore`
-  # @return [Array]
-  def fetch_array_values(type)
-    keg_cache = database.get(keg_name)
-    return [] unless keg_cache
-    json_string_to_ruby_hash(keg_cache)["array_values"][type.to_s]
-  end
+  HASH_LINKAGE_TYPES = [:keg_files_dylibs].freeze
 
   # @param  [Symbol] type
   # @return [Hash]
   def fetch_hash_values(type)
-    keg_cache = database.get(keg_name)
+    keg_cache = database.get(@keg_name)
     return {} unless keg_cache
-    json_string_to_ruby_hash(keg_cache)["hash_values"][type.to_s]
-  end
-
-  # Formats the linkage data for `array_values` into a kind which can be parsed
-  # by the `json_string_to_ruby_hash` method. Internally converts ruby `Set`s to
-  # `Array`s
-  #
-  # @param  [Hash]
-  # @return [String]
-  def format_array_values(hash)
-    hash.each_with_object({}) { |(k, v), h| h[k] = v.to_a }
-  end
-
-  # Formats the linkage data for `hash_values` into a kind which can be parsed
-  # by the `json_string_to_ruby_hash` method. Converts ruby `Set`s to `Array`s,
-  # and converts ruby `Pathname`s to `String`s
-  #
-  # @param  [Hash]
-  # @return [String]
-  def format_hash_values(hash)
-    hash.each_with_object({}) do |(outer_key, outer_values), outer_hash|
-      outer_hash[outer_key] = outer_values.each_with_object({}) do |(k, v), h|
-        h[k] = v.to_a.map(&:to_s)
-      end
-    end
+    json_string_to_ruby_hash(keg_cache)[type.to_s]
   end
 end
