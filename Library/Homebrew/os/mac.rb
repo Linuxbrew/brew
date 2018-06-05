9640ef0540f22f62d12e401ae9d164fe44309e83
Dominyk Tiller <dominyktiller@gmail.com>
2018-06-05 06:48:26 +0100

    mac: add 9.4 & 10.0 compilers

--- Library/Homebrew/os/mac.rb
+++ Library/Homebrew/os/mac.rb
@@ -211,6 +211,8 @@ module OS
       "9.1"   => { clang: "9.0", clang_build: 900 },
       "9.2"   => { clang: "9.0", clang_build: 900 },
       "9.3"   => { clang: "9.1", clang_build: 902 },
+      "9.4"   => { clang: "9.1", clang_build: 902 },
+      "10.0"  => { clang: "10.0", clang_build: 1000 },
     }.freeze
 
     def compilers_standard?
