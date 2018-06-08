c68526ac09261f29bf261b530a5d449544380ecb
commitay <commitay@users.noreply.github.com>
2018-06-09 08:28:03 +1000

    audit: remove appcast checkpoints

--- Library/Homebrew/test/support/fixtures/cask/Casks/appcast-with-checkpoint.rb
+++ Library/Homebrew/test/support/fixtures/cask/Casks/appcast-with-checkpoint.rb
@@ -1,4 +1,4 @@
-cask 'appcast-valid-checkpoint' do
+cask 'appcast-with-checkpoint' do
   appcast 'http://localhost/appcast.xml',
           checkpoint: 'd5b2dfbef7ea28c25f7a77cd7fa14d013d82b626db1d82e00e25822464ba19e2'
 end
