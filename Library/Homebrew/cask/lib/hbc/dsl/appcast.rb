c3ac638b9f288b7090f2e37004cdc30e35cbfb74
commitay <commitay@users.noreply.github.com>
2018-06-09 08:45:13 +1000

    appcast: remove `calculate_checkpoint`

--- Library/Homebrew/cask/lib/hbc/dsl/appcast.rb
+++ Library/Homebrew/cask/lib/hbc/dsl/appcast.rb
@@ -11,24 +11,6 @@ module Hbc
         @checkpoint = parameters[:checkpoint]
       end
 
-      def calculate_checkpoint
-        curl_executable, *args = curl_args(
-          "--compressed", "--location", "--fail", uri,
-          user_agent: :fake
-        )
-        result = SystemCommand.run(curl_executable, args: args, print_stderr: false)
-
-        checkpoint = if result.success?
-          processed_appcast_text = result.stdout.gsub(%r{<pubDate>[^<]*</pubDate>}m, "")
-          Digest::SHA2.hexdigest(processed_appcast_text)
-        end
-
-        {
-          checkpoint: checkpoint,
-          command_result: result,
-        }
-      end
-
       def to_yaml
         [uri, parameters].to_yaml
       end
