6d1f99fa5f7c5dbd06d4db1aed5ea368073103b3
commitay <commitay@users.noreply.github.com>
2018-06-15 16:16:55 +1000

    cask info: display `auto_updates`

---
+++ Library/Homebrew/test/support/fixtures/cask/Casks/with-auto-updates.rb
@@ -0,0 +1,12 @@
+cask 'with-auto-updates' do
+  version '1.0'
+  sha256 'e5be907a51cd0d5b128532284afe1c913608c584936a5e55d94c75a9f48c4322'
+
+  url "https://example.com/autoupdates_#{version}.zip"
+  name 'AutoUpdates'
+  homepage 'http://example.com/autoupdates'
+
+  auto_updates true
+
+  app 'AutoUpdates.app'
+end
