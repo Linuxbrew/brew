b12498159bce95aed7b03332cfe52dfec7a6451c
commitay <commitay@users.noreply.github.com>
2018-06-15 17:01:27 +1000

    cask audit: check for devmate and hockeyapp appcasts

---
+++ Library/Homebrew/test/support/fixtures/cask/Casks/devmate-without-appcast.rb
@@ -0,0 +1,11 @@
+cask 'devmate-without-appcast' do
+  version '1.0'
+  sha256 'a69e7357bea014f4c14ac9699274f559086844ffa46563c4619bf1addfd72ad9'
+
+  # dl.devmate.com/com.my.fancyapp was verified as official when first introduced to the cask
+  url "https://dl.devmate.com/com.my.fancyapp/app_#{version}.zip"
+  name 'DevMate'
+  homepage 'http://www.example.com/'
+
+  app 'DevMate.app'
+end
