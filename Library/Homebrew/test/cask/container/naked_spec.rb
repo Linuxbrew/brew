a9e109e31a9b962c0ea5edc36a36b0296b3fb229
Markus Reiter <me@reitermark.us>
2018-07-14 02:26:59 +0200

    Don’t expand `executable` path in `SystemCommand`.

--- Library/Homebrew/test/cask/container/naked_spec.rb
+++ Library/Homebrew/test/cask/container/naked_spec.rb
@@ -7,15 +7,15 @@ describe Hbc::Container::Naked, :cask do
 
     path                 = "/tmp/downloads/kevin-spacey-1.2.pkg"
     expected_destination = cask.staged_path.join("kevin spacey.pkg")
-    expected_command     = ["/usr/bin/ditto", "--", path, expected_destination]
-    Hbc::FakeSystemCommand.stubs_command(expected_command)
 
     container = Hbc::Container::Naked.new(cask, path, Hbc::FakeSystemCommand)
+
+    Hbc::FakeSystemCommand.expects_command(
+      ["/usr/bin/ditto", "--", path, expected_destination],
+    )
 
     expect {
       container.extract
     }.not_to raise_error
-
-    expect(Hbc::FakeSystemCommand.system_calls[expected_command]).to eq(1)
   end
 end
