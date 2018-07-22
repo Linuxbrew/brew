4f495a967d9cccd123891136348e41bda10d3902
Markus Reiter <me@reitermark.us>
2018-07-22 19:30:16 +0200

    Add `verbose` to `extract_nestedly`.

--- Library/Homebrew/download_strategy.rb
+++ Library/Homebrew/download_strategy.rb
@@ -48,7 +48,8 @@ class AbstractDownloadStrategy
   # Unlike {Resource#stage}, this does not take a block.
   def stage
     UnpackStrategy.detect(cached_location, ref_type: @ref_type, ref: @ref)
-                  .extract_nestedly(basename: basename_without_params)
+                  .extract_nestedly(basename: basename_without_params,
+                                    verbose: ARGV.verbose? && !shutup)
   end
 
   # @!attribute [r] cached_location
@@ -68,7 +69,7 @@ class AbstractDownloadStrategy
   end
 
   def safe_system(*args)
-    if @shutup
+    if shutup
       return if quiet_system(*args)
       raise(ErrorDuringExecution.new(args, status: $CHILD_STATUS))
     else
