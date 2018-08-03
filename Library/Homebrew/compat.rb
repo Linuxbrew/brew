e0e750e458943751e06a66fdcce2bf475ecdca5b
Markus Reiter <me@reitermark.us>
2018-08-03 22:21:20 +0200

    Remove `compat/download_strategy`.

--- Library/Homebrew/compat.rb
+++ Library/Homebrew/compat.rb
@@ -1,7 +1,6 @@
 require "compat/os/mac"
 require "compat/dependable"
 require "compat/dependency_collector"
-require "compat/download_strategy"
 require "compat/fileutils"
 require "compat/formula_support"
 require "compat/hbc"
