1003d722bd3b685ec50a4781227e4a5a413a462d
Markus Reiter <me@reitermark.us>
2018-08-03 18:38:30 +0200

    Use `--` to separate download name and version.

--- Library/Homebrew/test/cmd/fetch_spec.rb
+++ Library/Homebrew/test/cmd/fetch_spec.rb
@@ -2,10 +2,10 @@ describe "brew fetch", :integration_test do
   it "downloads the Formula's URL" do
     setup_test_formula "testball"
 
-    expect(HOMEBREW_CACHE/"testball-0.1.tbz").not_to exist
+    expect(HOMEBREW_CACHE/"testball--0.1.tbz").not_to exist
 
     expect { brew "fetch", "testball" }.to be_a_success
 
-    expect(HOMEBREW_CACHE/"testball-0.1.tbz").to exist
+    expect(HOMEBREW_CACHE/"testball--0.1.tbz").to exist
   end
 end
