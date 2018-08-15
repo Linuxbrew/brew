520b5a103ad251307b92403d28f74dc245769850
JBallin <JBallin@users.noreply.github.com>
2018-08-15 09:53:20 -0700

    Clarify that --cleanup only runs on formulae which were just upgraded

--- Library/Homebrew/cmd/upgrade.rb
+++ Library/Homebrew/cmd/upgrade.rb
@@ -4,7 +4,7 @@
 #:    Options for the `install` command are also valid here.
 #:
 #:    If `--cleanup` is specified or `HOMEBREW_UPGRADE_CLEANUP` is set then remove
-#:    previously installed <formulae> version(s).
+#:    previously installed version(s) of upgraded <formulae>.
 #:
 #:    If `--fetch-HEAD` is passed, fetch the upstream repository to detect if
 #:    the HEAD installation of the formula is outdated. Otherwise, the
