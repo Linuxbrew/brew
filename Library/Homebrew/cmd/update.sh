cc0f17eb7bbed39dc2c0367d6b7218485c38d317
Chongyu Zhu <i@lembacon.com>
2018-09-14 18:50:21 +0800

    cmd/update: make `curl` respect `HOMEBREW_CURLRC`

--- Library/Homebrew/cmd/update.sh
+++ Library/Homebrew/cmd/update.sh
@@ -406,6 +406,13 @@ EOS
     QUIET_ARGS=()
   fi
 
+  if [[ -z "$HOMEBREW_CURLRC" ]]
+  then
+    CURL_DISABLE_CURLRC_ARGS=(-q)
+  else
+    CURL_DISABLE_CURLRC_ARGS=()
+  fi
+
   # only allow one instance of brew update
   lock update
 
@@ -481,7 +488,9 @@ EOS
           GITHUB_API_ENDPOINT="commits/$UPSTREAM_BRANCH_DIR"
         fi
 
-        UPSTREAM_SHA_HTTP_CODE="$("$HOMEBREW_CURL" --silent --max-time 3 \
+        UPSTREAM_SHA_HTTP_CODE="$("$HOMEBREW_CURL" \
+           "${CURL_DISABLE_CURLRC_ARGS[@]}" \
+           --silent --max-time 3 \
            --location --output /dev/null --write-out "%{http_code}" \
            --dump-header "$DIR/.git/GITHUB_HEADERS" \
            --user-agent "$HOMEBREW_USER_AGENT_CURL" \
