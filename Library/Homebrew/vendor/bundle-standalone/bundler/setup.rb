a6f25419aaaeb5fae3eb743d961ed7892ae86075
Mike McQuaid <mike@mikemcquaid.com>
2018-09-14 10:08:43 +0100

    Update vendored gems
    - backports to 3.11.4
    - plist to 3.4.0

--- Library/Homebrew/vendor/bundle-standalone/bundler/setup.rb
+++ Library/Homebrew/vendor/bundle-standalone/bundler/setup.rb
@@ -3,7 +3,7 @@ require 'rbconfig'
 ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/backports-3.8.0/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/backports-3.11.4/lib"
 $:.unshift "#{path}/"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/plist-3.3.0/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/plist-3.4.0/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/ruby-macho-2.0.0/lib"
