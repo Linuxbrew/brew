9e438991c82359d184902a13824c407b429bc33e
L. E. Segovia <13498015+amyspark@users.noreply.github.com>
2018-10-08 09:44:03 +0000

    zip: restore extended attributes when under MacOS

--- Library/Homebrew/extend/os/mac/unpack_strategy/zip.rb
+++ Library/Homebrew/extend/os/mac/unpack_strategy/zip.rb
@@ -2,10 +2,21 @@ module UnpackStrategy
   class Zip
     prepend Module.new {
       def extract_to_dir(unpack_dir, basename:, verbose:)
-        volumes = super.stderr.chomp
-                       .split("\n")
-                       .map { |l| l[/\A   skipping: (.+)  volume label\Z/, 1] }
-                       .compact
+        result = super
+
+        volumes = result.stderr.chomp
+                        .split("\n")
+                        .map { |l| l[/\A   skipping: (.+)  volume label\Z/, 1] }
+                        .compact
+
+        if result.stderr.lines.any? { |line| line.start_with?("._") }
+          # Merge ._ files back into extended attributes.
+          # ._ files inside volumes are automatically merged by ditto.
+          system_command!("dot_clean",
+                          args: ["-mv", "--keep=dotbar", unpack_dir],
+                          verbose: verbose,
+                          print_stderr: false)
+        end
 
         return if volumes.empty?
 
