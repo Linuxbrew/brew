599ecc9b5ad7951b8ddc51490ebe93a976d43b29
Mike McQuaid <mike@mikemcquaid.com>
2018-10-14 21:10:15 -0700

    Deprecate unused download strategies
    Download the unused, private download strategies. These are a better fit
    for being declared in the formulae and/or taps that use them rather than
    in Homebrew/brew where they are unused by Homebrew maintainers or
    official taps/formulae.

--- Library/Homebrew/compat.rb
+++ Library/Homebrew/compat.rb
@@ -1,6 +1,7 @@
 require "compat/os/mac"
 require "compat/dependable"
 require "compat/dependency_collector"
+require "compat/download_strategy"
 require "compat/fileutils"
 require "compat/formula_support"
 require "compat/cask"
