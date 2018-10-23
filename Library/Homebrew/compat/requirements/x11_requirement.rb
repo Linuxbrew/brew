5bea36799bf7f44b0ced62adf7b90b0dacc5381e
Mike McQuaid <mike@mikemcquaid.com>
2018-10-23 11:09:04 +0100

    compat/requirements: add odeprecated.
    These can be enabled now Homebrew/homebrew-core has been fixed
    (in https://github.com/Homebrew/homebrew-core/pull/32968).

--- Library/Homebrew/compat/requirements/x11_requirement.rb
+++ Library/Homebrew/compat/requirements/x11_requirement.rb
@@ -4,8 +4,7 @@ class X11Requirement < Requirement
   module Compat
     def initialize(tags = [])
       if tags.first.to_s.match?(/(\d\.)+\d/)
-        # TODO: deprecate when Homebrew/homebrew-core is fixed.
-        # odeprecated('depends_on :x11 => "X.Y.Z"')
+        odeprecated('depends_on :x11 => "X.Y.Z"')
         tags.shift
       end
 
