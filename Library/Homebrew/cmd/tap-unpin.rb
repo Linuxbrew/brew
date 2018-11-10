12040c2525ddb8157eeb0b18c9e9172d137df49a
Gautham Goli <goligautham@gmail.com>
2018-11-10 23:31:23 +0530

    tap-unpin: Use CLI::Parser to parse args

--- Library/Homebrew/cmd/tap-unpin.rb
+++ Library/Homebrew/cmd/tap-unpin.rb
@@ -1,10 +1,25 @@
 #:  * `tap-unpin` <tap>:
 #:    Unpin <tap> so its formulae are no longer prioritized. See also `tap-pin`.
 
+require "cli_parser"
+
 module Homebrew
   module_function
 
+  def tap_unpin_args
+    Homebrew::CLI::Parser.new do
+      usage_banner <<~EOS
+        `tap-unpin` <tap>
+
+        Unpin <tap> so its formulae are no longer prioritized. See also `tap-pin`.
+      EOS
+      switch :debug
+    end
+  end
+
   def tap_unpin
+    tap_unpin_args.parse
+
     ARGV.named.each do |name|
       tap = Tap.fetch(name)
       raise "unpinning #{tap} is not allowed" if tap.core_tap?
