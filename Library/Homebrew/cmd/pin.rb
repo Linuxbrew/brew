f9883a922218158944615efed9217ab7ae93caa8
Gautham Goli <goligautham@gmail.com>
2018-11-11 20:06:40 +0530

    pin: Use CLI::Parser to parse args

--- Library/Homebrew/cmd/pin.rb
+++ Library/Homebrew/cmd/pin.rb
@@ -3,12 +3,27 @@
 #:    issuing the `brew upgrade` <formulae> command. See also `unpin`.
 
 require "formula"
+require "cli_parser"
 
 module Homebrew
   module_function
 
+  def pin_args
+    Homebrew::CLI::Parser.new do
+      usage_banner <<~EOS
+        `pin` <formulae>
+
+        Pin the specified <formulae>, preventing them from being upgraded when
+        issuing the `brew upgrade` <formulae> command. See also `unpin`.
+      EOS
+      switch :debug
+    end
+  end
+
   def pin
-    raise FormulaUnspecifiedError if ARGV.named.empty?
+    pin_args.parse
+
+    raise FormulaUnspecifiedError if args.remaining.empty?
 
     ARGV.resolved_formulae.each do |f|
       if f.pinned?
