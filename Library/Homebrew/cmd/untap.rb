e70df22acd43cea2f065d3a8497d9e966dda4579
Gautham Goli <goligautham@gmail.com>
2018-11-11 18:35:55 +0530

    untap: Use CLI::Parser to parse args

--- Library/Homebrew/cmd/untap.rb
+++ Library/Homebrew/cmd/untap.rb
@@ -1,11 +1,26 @@
 #:  * `untap` <tap>:
 #:    Remove a tapped repository.
 
+require "cli_parser"
+
 module Homebrew
   module_function
 
+  def untap_args
+    Homebrew::CLI::Parser.new do
+      usage_banner <<~EOS
+        `untap` <tap>
+
+        Remove a tapped repository.
+      EOS
+      switch :debug
+    end
+  end
+
   def untap
-    raise "Usage is `brew untap <tap-name>`" if ARGV.empty?
+    untap_args.parse
+
+    raise "Usage is `brew untap <tap-name>`" if args.remaining.empty?
 
     ARGV.named.each do |tapname|
       tap = Tap.fetch(tapname)
