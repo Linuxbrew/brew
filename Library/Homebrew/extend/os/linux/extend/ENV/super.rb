9769fb22fe2cef2dc00420111fd4ecf5b19e5c0d
Maxim Belkin <maxim.belkin@gmail.com>
2018-11-17 10:27:05 +0000

    Remove Xorg-related hacks from superenv on Linux
    Pull Request: Linuxbrew/brew#868

--- Library/Homebrew/extend/os/linux/extend/ENV/super.rb
+++ Library/Homebrew/extend/os/linux/extend/ENV/super.rb
@@ -24,7 +24,6 @@ module Superenv
         nil
       end
     end.compact
-    paths += xorg_recursive_deps.map(&:opt_bin) if x11?
     paths
   end
 
@@ -38,82 +37,5 @@ module Superenv
 
   def determine_dynamic_linker_path
     "#{HOMEBREW_PREFIX}/lib/ld.so"
-  end
-
-  # @private
-  def xorg_recursive_deps
-    return [] unless xorg_installed?
-    @xorg_deps ||= Formula["linuxbrew/xorg/xorg"].recursive_dependencies.map(&:to_formula)
-  rescue FormulaUnavailableError
-    []
-  end
-
-  # @private
-  def homebrew_extra_pkg_config_paths
-    paths = []
-    if x11?
-      libs = xorg_recursive_deps.map(&:lib)
-      shares = xorg_recursive_deps.map(&:share)
-      libs.each do |lib|
-        paths << lib/"pkgconfig"
-      end
-      shares.each do |share|
-        paths << share/"pkgconfig"
-      end
-    end
-    paths
-  end
-
-  def homebrew_extra_aclocal_paths
-    paths = []
-    if x11?
-      shares = xorg_recursive_deps.map(&:share)
-      shares.each do |share|
-        paths << share/"aclocal"
-      end
-    end
-    paths
-  end
-
-  def xorg_include_paths
-    xorg_recursive_deps.map(&:include)
-  end
-
-  def xorg_lib_paths
-    xorg_recursive_deps.map(&:lib)
-  end
-
-  def homebrew_extra_isystem_paths
-    paths = []
-    paths += xorg_include_paths if x11?
-    paths
-  end
-
-  def homebrew_extra_library_paths
-    paths = []
-    paths += xorg_lib_paths if x11?
-    paths
-  end
-
-  def homebrew_extra_cmake_include_paths
-    paths = []
-    paths += xorg_include_paths if x11?
-    paths
-  end
-
-  def homebrew_extra_cmake_library_paths
-    paths = []
-    paths += xorg_lib_paths if x11?
-    paths
-  end
-
-  def set_x11_env_if_installed
-    ENV.x11 = xorg_installed?
-  end
-
-  def xorg_installed?
-    Formula["linuxbrew/xorg/xorg"].installed?
-  rescue FormulaUnavailableError
-    false
   end
 end
