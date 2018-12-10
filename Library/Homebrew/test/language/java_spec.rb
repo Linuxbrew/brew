e659ce9cd2a81efa0425ebb51f6e1a892ff75ab9
Shaun Jackman <sjackman@gmail.com>
2018-12-10 11:06:55 -0800

    Revert test/language/java to upstream [Linux]

--- Library/Homebrew/test/language/java_spec.rb
+++ Library/Homebrew/test/language/java_spec.rb
@@ -16,40 +16,24 @@ describe Language::Java do
   describe "::java_home_env" do
     it "returns java_home path with version if version specified", :needs_macos do
       java_home = described_class.java_home_env("blah")
-      if OS.mac?
-        expect(java_home[:JAVA_HOME]).to include("--version blah")
-      else
-        expect(java_home[:JAVA_HOME]).to eq(nil)
-      end
+      expect(java_home[:JAVA_HOME]).to include("--version blah")
     end
 
     it "returns java_home path without version if version is not specified", :needs_java do
       java_home = described_class.java_home_env
-      if OS.mac?
-        expect(java_home[:JAVA_HOME]).not_to include("--version")
-      else
-        expect(java_home[:JAVA_HOME]).to eq(nil)
-      end
+      expect(java_home[:JAVA_HOME]).not_to include("--version")
     end
   end
 
   describe "::overridable_java_home_env" do
     it "returns java_home path with version if version specified", :needs_macos do
       java_home = described_class.overridable_java_home_env("blah")
-      if OS.mac?
-        expect(java_home[:JAVA_HOME]).to include("--version blah")
-      else
-        expect(java_home[:JAVA_HOME]).to eq(nil)
-      end
+      expect(java_home[:JAVA_HOME]).to include("--version blah")
     end
 
     it "returns java_home path without version if version is not specified", :needs_java do
       java_home = described_class.overridable_java_home_env
-      if OS.mac?
-        expect(java_home[:JAVA_HOME]).not_to include("--version")
-      else
-        expect(java_home[:JAVA_HOME]).to eq(nil)
-      end
+      expect(java_home[:JAVA_HOME]).not_to include("--version")
     end
   end
 end
