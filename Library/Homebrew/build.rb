b7d84f2d9e6b0c44c712dab7987ee3244f80062a
William Woodruff <william@yossarian.net>
2018-12-23 11:24:48 -0500

    build: Propagate entire ErrorDuringExecution
    Fixes https://github.com/Homebrew/brew/issues/5427.

--- Library/Homebrew/build.rb
+++ Library/Homebrew/build.rb
@@ -202,6 +202,10 @@ rescue Exception => e # rubocop:disable Lint/RescueException
     error_hash["cmd"] = e.cmd
     error_hash["args"] = e.args
     error_hash["env"] = e.env
+  elsif error_hash["json_class"] = "ErrorDuringExecution"
+    error_hash["cmd"] = e.cmd
+    error_hash["status"] = e.status.exitstatus
+    error_hash["output"] = e.output
   end
 
   error_pipe.puts error_hash.to_json
